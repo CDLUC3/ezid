575d0c420e1234a1696f931e150e1c560ac92764
scott fisher <scott.fisher@ucop.edu>
2011-12-02 11:55:21 -0800

    Added all the home page documentation pages and the text that was available.

--- code/ui_home.py
+++ code/ui_home.py
@@ -3,3 +3,20 @@ from django.shortcuts import render_to_response
 def home(request):
   return render_to_response('home/home.html')
 
+def community(request):
+  return render_to_response('home/community.html')
+
+def documentation(request):
+  return render_to_response('home/documentation.html')
+
+def outreach(request):
+  return render_to_response('home/outreach.html')
+
+def pricing(request):
+  return render_to_response('home/pricing.html')
+
+def understanding(request):
+  return render_to_response('home/understanding.html')
+
+def why(request):
+  return render_to_response('home/why.html')
