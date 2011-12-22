a735d58f5df649a542b3faa36a8b9bdba3809d2c
scott fisher <scott.fisher@ucop.edu>
2011-12-21 16:08:47 -0800

    Creating simple create id screen.

--- code/ui_create.py
+++ code/ui_create.py
@@ -1,5 +1,6 @@
 import ui_common as uic
 from django.shortcuts import render_to_response, redirect
+import metadata
 
 d = { 'menu_item' : 'ui_create.null'}
 
@@ -10,6 +11,8 @@ def index(request):
 
 def simple(request):
   d['menu_item'] = 'ui_create.simple'
+  d['current_profile'] = metadata.getProfile('dc')
+  d['internal_profile'] = metadata.getProfile('internal')
   return uic.render(request, 'create/simple', d)
 
 def advanced(request):
