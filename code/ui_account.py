245ade52426954381233c61df1dd2aa1b8eab4b1
scott fisher <scott.fisher@ucop.edu>
2012-02-03 10:34:35 -0800

    Fixing co-owner showing up strange.

--- code/ui_account.py
+++ code/ui_account.py
@@ -1,6 +1,7 @@
 import ui_common as uic
 import userauth, useradmin
 import django.contrib.messages
+import idmap
 from django.shortcuts import redirect
 
 d = { 'menu_item' : 'ui_null.null'}
@@ -21,9 +22,12 @@ def edit(request):
       return redirect("ui_home.index")
     r.update(r2)
     d.update(r)
-    
+    if not 'ezidCoOwners' in d:
+      d['ezidCoOwners'] = ''
+      
   elif request.method == "POST":
-    pass
+    if validate_edit_user(request):
+      print "This would now save the form."
   return uic.render(request, "account/edit", d)
 
 def login(request):
@@ -70,4 +74,44 @@ def logout(request):
 
 def contact(request):
   pass
+
+
+def validate_edit_user(request):
+  """validates that the fields required to update a user are set"""
+  valid_form = True
+  fields = ['givenName', 'sn', 'mail', 'telephoneNumber', 'ezidCoOwners', 'pwcurrent','pwnew', 'pwconfirm']
+  
+  for field in fields:
+    if not field in request.POST:
+      django.contrib.messages.error(request, "Form submission error.")
+      return False
+  
+  required_fields = {'givenName': 'First name', 'sn': 'Last name', 'mail': 'Email address'}
+  for field in required_fields:
+    if request.POST[field].strip() == '':
+      django.contrib.messages.error(request, required_fields[field] + " must be filled in.")
+      valid_form = False
+  
+  if request.POST['ezidCoOwners'] != '':
+    coowners = [co.strip() for co in request.POST['ezidCoOwners'].split(',')]
+    for coowner in coowners:
+      try:
+        idmap.getUserId(coowner)
+      except AssertionError:
+        django.contrib.messages.error(request, coowner + " is not a correct username for a co-owner.")
+        valid_form = False
+  
+  if not request.POST['pwcurrent'].strip() == '':
+    auth = userauth.authenticate(request.session['auth'].user[0], request.POST["pwcurrent"])
+    if type(auth) is str or not auth:
+      django.contrib.messages.error(request, "Your current password is incorrect.")
+      valid_form = False
+    if request.POST['pwnew'] != request.POST['pwconfirm']:
+      django.contrib.messages.error(request, "Your new and confirmed new passwords do not match.")
+      valid_form = False
+    if request.POST['pwnew'] == '' or request.POST['pwconfirm'] == '':
+      django.contrib.messages.error(request, "Your new password cannot be empty.")
+      valid_form = False
+  return valid_form
+  
   
