b645e91a4bb943bc2c677e344bee995a6717b7b5
sfisher <devnull@localhost>
2012-03-21 13:37:55 -0700

    Fixing and generalizing dialog box javascript.

--- static/javascripts/dialog_box.js
+++ static/javascripts/dialog_box.js
@@ -1,20 +1,21 @@
 /*
  * This will allow modal dialog boxes.
  * 
- * Create a link to open the dialog with the following properties:
- * name="modal"   href="#<id_of_div_to_show>"
+ * Create a div to contain your dialog box like this, %value% represents a value you substitute:
  * 
- * The dialog div should be like so:
- * <div class="dialog_window" id="<id_of_div_as_in_above>">
- *   
- *   Closing link inside the dialog:
- *   <a class="close">close me</a>
- * 
+ * <div id="%div_id%" class="dialog_window">
+ * 	 . . . contents . . .
+ *   <a href="#close" class="close">Cancel</a>
  * </div>
+ * 
+ * Create a link to open the dialog like so:
+ * <a href="#%div_id%" name="modal">Open my dialog</a>
  * 
  */
 
 $(document).ready(function() {
+	
+	$('body').append('<div class="mask" id="mask"></div>');
 	
 	//select all the a tag with name equal to modal
   $('a[name=modal]').click(function(e) {
@@ -23,15 +24,19 @@ $(document).ready(function() {
     e.preventDefault();
     //Get the A tag
     var id = $(this).attr('href');
+    var bl = $(id);
     
-    var x = e.pageX + 20;
-    var y = e.pageY + 20;
+    // var x = e.pageX + 20;
+    //var y = e.pageY + 20;
+    var y = e.pageY - bl.height() / 2;
+    var x = ($(window).width() - bl.width()) / 2;
+    //var y = ($(window).height() - bl.height()) / 2;
 
     //transition effect   
     $('#mask').fadeIn(500);   
     $('#mask').fadeTo("fast",0.8); 
 
-    bl = $(id);
+    
     bl.css('position', 'absolute');
     bl.css( 'top', y);
     bl.css( 'left', x); 
@@ -53,8 +58,7 @@ $(document).ready(function() {
       $(this).hide();
       $('.dialog_window').hide();
     });
-    
-    $('body').append('<div class="mask" id="mask"></div>');    
+        
 });
 
 $(document).keyup(function(e) {
