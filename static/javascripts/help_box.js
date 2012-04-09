896fd516dd81952043010310a9f1cce502004d6e
sfisher <devnull@localhost>
2012-04-09 15:02:12 -0700

    work in progress on help javascript.

--- static/javascripts/help_box.js
+++ static/javascripts/help_box.js
@@ -4,20 +4,23 @@
  * Create a div to contain your help box like this, %value% represents a value you substitute:
  * 
  * <div id="%div_id%" class="help_window">
- * 	<a href="#close" class="close">Cancel</a>
  * 	 . . . contents . . .
- *   
  * </div>
  * 
- * Create a link to open the help dialog like so:
- * <a href="#%div_id%" name="help_dialog">Open my help</a>
+ * Create a link to open the help like so:
+ * <a href="#%div_id%" name="help_link">Open my help</a>
+ * 
+ * Note the div_id around the contents and the div_id in the href for the link must match,
+ * (though the div_id in the href has a # in front of it)
  * 
  */
 
 $(document).ready(function() {
 	
-	//select all the a tag with name equal to modal
-  $('a[name=help_dialog]').click(function(e) {
+	//add close buttons to all help_window classes
+	$('.help_window').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
+	//select all the a tag with name equal to help_link
+  $('a[name=help_link]').click(function(e) {
 
   	//Cancel the link behavior
     e.preventDefault();
@@ -25,14 +28,15 @@ $(document).ready(function() {
     var id = $(this).attr('href');
     var bl = $(id);
     
-    var x = e.pageX;
-    // var y = e.pageY + 20;
-    // var y = e.pageY - bl.height() / 2;
-    var y = e.pageY + 20;
-    // var y = e.pageY - bl.height() - 30;
-    // var x = ($(window).width() - bl.width()) / 2;
-    //var y = ($(window).height() - bl.height()) / 2;
-
+    
+    // option for putting above and centered on clicked link
+    var x = e.pageX - bl.width() / 2;
+    var y = e.pageY - bl.height() - 50;
+    if (y < 1){
+    	// option for putting below and right of clicked link
+    	var x = e.pageX; // left/right
+    	var y = e.pageY + 20; // up/down
+    }
     
     bl.css('position', 'absolute');
     bl.css( 'top', y);
@@ -40,14 +44,14 @@ $(document).ready(function() {
     //transition effect
     $(id).fadeIn(100);
  
-    });
+  });
      
-    //if close button is clicked
-    $('.help_window .close').click(function (e) {
-    	//Cancel the link behavior
-      e.preventDefault();
-      $('.help_window').hide();
-    });
+  //if close button is clicked
+  $('.help_window .close').click(function (e) {
+  	//Cancel the link behavior
+    e.preventDefault();
+    $('.help_window').hide();
+  });
         
 });
 
