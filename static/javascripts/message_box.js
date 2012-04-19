168a8199cd72eda160b00c81fba1da596a22bdcd
sfisher <devnull@localhost>
2012-04-19 16:51:23 -0700

    Formatting of error messages.

--- static/javascripts/message_box.js
+++ static/javascripts/message_box.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 	//alert($("#status:not(:empty)").length);
 	//alert($('status').innerHtml());
 	if ($.trim($("#status").html())){
-		$('#status').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
+		//$('#status').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
+		$('#status').prepend('<div class="close_button"><a href="#close" class="close">hide message</a></div>');
 		var x = $(window).width() / 2 - $('#status').width() / 2;
 		$('#status').css('position', 'absolute');
     $('#status').css( 'top', 20);
