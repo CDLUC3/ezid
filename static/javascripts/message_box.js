15c7cd23603676d3a8895f5fe22349249ec91391
sfisher <devnull@localhost>
2012-04-19 17:21:07 -0700

    auto-hide timer for alert message.

--- static/javascripts/message_box.js
+++ static/javascripts/message_box.js
@@ -5,12 +5,27 @@ $(document).ready(function() {
 	if ($.trim($("#status").html())){
 		//$('#status').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
 		$('#status').prepend('<div class="close_button"><a href="#close" class="close">hide message</a></div>');
+		/*var h = 0;
+		if($(window).height() < $(document).height()){
+			h = $(window).height();
+		}else{
+			h = $(document).height();
+			var y = h / 2 - $('#status').height() / 2 - 100;
+		}
+		 */
 		var x = $(window).width() / 2 - $('#status').width() / 2;
+		var y = 20;
+		
 		$('#status').css('position', 'absolute');
-    $('#status').css( 'top', 20);
+    $('#status').css( 'top', y);
     $('#status').css( 'left', x); 
     //transition effect
     $('#status').fadeIn(100);
+    
+    window.setTimeout(function() {
+ 			$('#status').fadeOut('slow');
+		}, $('#status').height() * 50);
+
     
     //if close button is clicked
   	$('#status .close').click(function (e) {
