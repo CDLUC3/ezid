f32732bbd56fb0adb21af7a4fa9b1651dc1015b7
sfisher <devnull@localhost>
2012-04-23 16:00:47 -0700

    Fixing conflicting #status css styles that were causing some weird things.  Renamed the status div #ustatus

--- static/javascripts/message_box.js
+++ static/javascripts/message_box.js
@@ -1,45 +1,45 @@
 //#status messages
 $(document).ready(function() {
-	//alert($("#status:not(:empty)").length);
-	//alert($('status').innerHtml());
-	if ($.trim($("#status").html())){
-		//$('#status').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
-		$('#status').prepend('<div class="close_button"><a href="#close" class="close">hide message</a></div>');
+	//alert($("#ustatus:not(:empty)").length);
+	//alert($('ustatus').innerHtml());
+	if ($.trim($("#ustatus").html())){
+		//$('#ustatus').prepend('<div class="close_button"><a href="#close" class="close"><img src="/ezid/static/images/close_it.png" alt="close button" title="close button"/></a></div>');
+		$('#ustatus').prepend('<div class="close_button"><a href="#close" class="close">hide message</a></div>');
 		/*var h = 0;
 		if($(window).height() < $(document).height()){
 			h = $(window).height();
 		}else{
 			h = $(document).height();
-			var y = h / 2 - $('#status').height() / 2 - 100;
+			var y = h / 2 - $('#ustatus').height() / 2 - 100;
 		}
 		 */
-		var x = $(window).width() / 2 - $('#status').width() / 2;
+		var x = $(window).width() / 2 - $('#ustatus').width() / 2;
 		var y = 20;
 		
-		$('#status').css('position', 'absolute');
-    $('#status').css( 'top', y);
-    $('#status').css( 'left', x); 
+		$('#ustatus').css('position', 'absolute');
+    $('#ustatus').css( 'top', y);
+    $('#ustatus').css( 'left', x); 
     //transition effect
-    $('#status').fadeIn(100);
+    $('#ustatus').fadeIn(100);
     
     if(autohide){
     	window.setTimeout(function() {
- 				$('#status').fadeOut('slow');
-			}, $('#status').height() * 50);
+ 				$('#ustatus').fadeOut('slow');
+			}, $('#ustatus').height() * 50);
 		}
 
     
     //if close button is clicked
-  	$('#status .close').click(function (e) {
+  	$('#ustatus .close').click(function (e) {
   		//Cancel the link behavior
     	e.preventDefault();
-    	$('#status').hide();
+    	$('#ustatus').hide();
     	$.post('/ezid/account/ajax_hide_alert');
   	});
   	
 		$(document).keyup(function(e) {
 		  if(e.keyCode == 27) { // escape key
-		    $('#status').hide();
+		    $('#ustatus').hide();
 		    $.post('/ezid/account/ajax_hide_alert');
 		  }
 		});
