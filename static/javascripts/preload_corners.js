16d8e48079c2551b577adeb3bf8d143f2980547e
sfisher <devnull@localhost>
2012-04-27 13:00:22 -0700

    Code to preload corner images because they do not always position correctly on the first load for Chrome/Safari.

---
+++ static/javascripts/preload_corners.js
@@ -0,0 +1,8 @@
+image1 = new Image(6,6);
+image1.src = "/ezid/static/images/corners/tl.gif";
+image2 = new Image(6,6)
+image2.src = "/ezid/static/images/corners/tr.gif";
+image3 = new Image(6,6);
+image3.src = "/ezid/static/images/corners/bl.gif";
+image4 = new Image(6, 6);
+image4.src = "/ezid/static/images/corners/br.gif";
