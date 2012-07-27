3c78dddc41f0b9e047dcbb8750368d25fb6f52d4
Scott Fisher <sfisher@ucop.edu>
2012-07-27 14:54:09 -0700

    preliminary development settings to test different domain names.

--- settings/development.py
+++ settings/development.py
@@ -1,3 +1,10 @@
 from common import *
 
 DEPLOYMENT_LEVEL = "development"
+
+#This tells a special template tag to substitute
+#one templates for another if the host name is in this dictionary
+HOST_TEMPLATE_CUSTOMIZATION = {'n2t-dev.cdlib.org:18880': 'purdue'}
+
+#tells contact-us mailer to mail to different address if domain in this dictionary
+HOST_EMAIL_CUSTOMIZATION = {'n2t-dev.cdlib.org:18880': 'scott.fisher@ucop.edu'}
