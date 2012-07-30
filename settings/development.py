276968a9308286c5e41eaf72a28f327529fd7fe9
Scott Fisher <sfisher@ucop.edu>
2012-07-30 16:59:35 -0700

    Changing host to n2t-dev-alt.cdlib.org for purdue testing.

--- settings/development.py
+++ settings/development.py
@@ -3,8 +3,8 @@ from common import *
 DEPLOYMENT_LEVEL = "development"
 
 #This tells a special template tag to substitute
-#one templates for another if the host name is in this dictionary
-HOST_TEMPLATE_CUSTOMIZATION = {'n2t-dev.cdlib.org:18880': 'purdue'}
+#one template for another if the host name is in this dictionary
+HOST_TEMPLATE_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'purdue'}
 
 #tells contact-us mailer to mail to different address if domain in this dictionary
-HOST_EMAIL_CUSTOMIZATION = {'n2t-dev.cdlib.org:18880': 'scott.fisher@ucop.edu'}
+HOST_EMAIL_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'scott.fisher@ucop.edu'}
