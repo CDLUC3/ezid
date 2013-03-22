ec569700e178d6fad594b722b02624a31f867671
Scott Fisher <sfisher@ucop.edu>
2013-03-22 16:13:57 -0700

    Adding development cname for 'ezid.lib.purdue.edu'

--- settings/development.py
+++ settings/development.py
@@ -4,7 +4,9 @@ DEPLOYMENT_LEVEL = "development"
 
 #This tells a special template tag to substitute
 #one template for another if the host name is in this dictionary
-HOST_TEMPLATE_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'purdue'}
+HOST_TEMPLATE_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'purdue',
+                               'ezid.lib.purdue.edu': 'purdue'}
 
 #tells contact-us mailer to mail to different address if domain in this dictionary
-HOST_EMAIL_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'datacite@purdue.edu'}
+HOST_EMAIL_CUSTOMIZATION = {'n2t-dev-alt.cdlib.org': 'datacite@purdue.edu',
+                            'ezid.lib.purdue.edu': 'datacite@purdue.edu'}
