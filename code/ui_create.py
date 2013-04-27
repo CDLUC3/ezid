86b5e4a9f33291d2bdf09d433dd5dce5ed5aa8e9
Scott Fisher <sfisher@ucop.edu>
2013-04-26 19:03:05 -0700

    mostly working parsing of datacite form and creation of XML document.

--- code/ui_create.py
+++ code/ui_create.py
@@ -5,6 +5,8 @@ import metadata
 import ezid
 import logging
 import urllib
+import re
+from lxml import etree
 
 def index(request):
   d = { 'menu_item' : 'ui_create.index'}
@@ -147,5 +149,89 @@ def advanced_form_processing(request, d):
 
 def _generate_datacite_xml(request):
   """This generates datacite XML from a form POST request and returns it"""
-  print request
+  r = etree.fromstring('<resource xmlns="http://datacite.org/schema/kernel-2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://datacite.org/schema/kernel-2.2 http://schema.datacite.org/meta/kernel-2.2/metadata.xsd"/>')
+
+  items = request.POST.items()
+  items = [x for x in items if x[0].startswith("/resource") ]
+  RESOURCE_ORDER = ["/resource/" + x for x in ['creators', 'titles', 'publisher', 'publicationYear', 'subjects', 
+                    'contributors', 'dates', 'language', 'resourceType', 'alternateIdentifiers',
+                    'relatedIdentifiers', 'rights', 'descriptions'] ]
+  items = sorted(items, key=lambda i: i[0].split("/")) #sort by element name
+  items = sorted(items, key=lambda i: len(i[0].split("/"))) #sort by element length
+  items = sorted(items, key=lambda i: RESOURCE_ORDER.
+                 index(re.search('^/resource/[a-zA-Z0-9\[\]]+', i[0]).group())) #sort in preferred order of sections
+
+  for k, v in items:
+    if v != '':
+      _create_xml_element(r, k, v)
+      print k + "=" + v
+      #print etree.tostring(r, pretty_print=True)
+      #print ''
+  
+  #child = etree.Element('child')
+  #r.append(child)
+  #child.text = 'some text'
+    
+  print etree.tostring(r, pretty_print=True)
   return ''
+
+def _create_xml_element(root_el, path, value):
+  """ creates xml element """
+  split_path = path.split("/")[1:]
+  if split_path[0] == _remove_ns(root_el.tag):
+    split_path = split_path[1:]
+  _xml_create(root_el, split_path, value)
+  
+def _xml_create(base_el, path_arr, value):
+  #create or check element here
+  tag = path_arr[0]
+  simple_tag = _remove_at(_remove_ordinal(tag))
+  children = _children_w_tag(base_el, simple_tag)
+  if _is_attribute(tag):
+    #set an attribute
+    if len(path_arr) > 1:
+      #this shouldn't happen
+      return
+    else:
+      base_el.set(simple_tag, value)
+  else:
+    if _has_ordinal(tag):
+      ord = _get_ordinal(tag) - 1 #must subtract one because form ordinals start at 1, not 0
+    else:
+      ord = 0  #no ordinal, then it's effectively an ordinal of the first [0]
+    if ord > len(children) - 1:
+      child = etree.Element(simple_tag)
+      base_el.append(child)
+    else:
+      child = children[ord]
+    if len(path_arr) == 1:
+      child.text = value  #leaf node, so set value
+    else:
+      _xml_create(child, path_arr[1:], value) #not leaf node, so recurse for more path
+
+def _remove_ns(str):
+  """ removes the horrifying namespace uri from string so can compare easily"""
+  return re.sub(r'^{[^}]+}', '', str)
+
+def _children_w_tag(base, tag):
+  return [child for child in base if _remove_ns(child.tag) == tag ]
+
+def _remove_ordinal(str):
+  """removes the ordinal like [1] from path element"""
+  return re.sub(r'\[[0-9]+\]$', '', str)
+
+def _has_ordinal(str):
+  """sees if it has an ordinal like [1] at end of element"""
+  return str.endswith("]")
+
+def _get_ordinal(str):
+  """gets the ordinal at end of element string like [1] and returns an integer"""
+  return int(re.search('\[([0-9]+)\]$', str).group(1))
+
+def _is_attribute(str):
+  """sees if this element string is an attribute"""
+  return str.startswith("@")
+
+def _remove_at(str):
+  """removes an @ sign at beginning of string if it exists"""
+  return re.sub(r'^@', '', str)
