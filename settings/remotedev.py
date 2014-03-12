798eb49de293b1c0a45b4d882105e2dc0686ff9e
Greg Janee <gjanee@ucop.edu>
2014-03-12 08:54:07 -0700

    Renamed the localdev deployment level to remotedev, and created a new localdev deployment level.

--- settings/remotedev.py
+++ settings/remotedev.py
@@ -1,10 +1,13 @@
+import ldap
+
 from common import *
 
-DEPLOYMENT_LEVEL = "localdev"
+DEPLOYMENT_LEVEL = "remotedev"
 
 STANDALONE = True
 SSL = False
-RELOAD_TEMPLATES = True
+
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
 ALLOWED_HOSTS = ["localhost"]
 LOCALIZATIONS["localhost:8001"] = ("purdue", ["gjanee@ucop.edu"])
