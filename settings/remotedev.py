df3e94191f29d0ca0e28452f48f4af5d10d97881
Andy Mardesich <andy.mardesich@ucop.edu>
2014-04-09 16:56:14 -0700

    66256578 Jisc-Edina branding

--- settings/remotedev.py
+++ settings/remotedev.py
@@ -11,3 +11,4 @@ ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
 ALLOWED_HOSTS = ["localhost"]
 LOCALIZATIONS["localhost:8001"] = ("purdue", ["gjanee@ucop.edu"])
+LOCALIZATIONS["localhost:8002"] = ("jisc-edina", ["gjanee@ucop.edu"])
