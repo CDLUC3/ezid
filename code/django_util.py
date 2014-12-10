d71cb1a696128f636301e7e84068d950c3041318
Greg Janee <gjanee@ucop.edu>
2014-12-10 11:43:28 -0800

    Made a huge performance improvement to django_util.deleteSessions.

--- code/django_util.py
+++ code/django_util.py
@@ -19,8 +19,9 @@ def deleteSessions (username):
   """
   Deletes all sessions for a given user.
   """
-  # Force instantiation of query set to limit database contention.
+  toDelete = []
   sessions = django.contrib.sessions.models.Session.objects.all()
   for s in sessions:
     d = s.get_decoded()
-    if "auth" in d and d["auth"].user[0] == username: s.delete()
+    if "auth" in d and d["auth"].user[0] == username: toDelete.append(s.pk)
+  sessions.filter(pk__in=toDelete).delete()
