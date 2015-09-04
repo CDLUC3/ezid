ef360d06d59443ecc9b0b6d7140c34765ac491a4
Greg Janee <gjanee@ucop.edu>
2015-09-03 18:18:47 -0700

    Added the delete-sessions tool.

--- code/django_util.py
+++ code/django_util.py
@@ -17,7 +17,8 @@ import django.contrib.sessions.models
 
 def deleteSessions (username):
   """
-  Deletes all sessions for a given user.
+  Deletes all sessions for a given user.  The number of sessions
+  deleted is returned.
   """
   toDelete = []
   sessions = django.contrib.sessions.models.Session.objects.all()
@@ -25,6 +26,8 @@ def deleteSessions (username):
     d = s.get_decoded()
     if "auth" in d and d["auth"].user[0] == username: toDelete.append(s.pk)
   # We'll hit an SQLite limit if we try to delete too many at once.
+  n = len(toDelete)
   while len(toDelete) > 0:
     sessions.filter(pk__in=toDelete[:900]).delete()
     toDelete = toDelete[900:]
+  return n
