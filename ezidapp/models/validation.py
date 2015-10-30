41ac01c4f2bc434d4eaa0b8acc5783dabcdde632
Greg Janee <gjanee@ucop.edu>
2015-10-30 08:13:50 -0700

    More validation functions.

--- ezidapp/models/validation.py
+++ ezidapp/models/validation.py
@@ -2,7 +2,7 @@
 #
 # EZID :: ezidapp/models/validation.py
 #
-# Common model validation functions.
+# Model validation functions.
 #
 # Author:
 #   Greg Janee <gjanee@ucop.edu>
@@ -51,5 +51,60 @@ maxDatacenterSymbolLength = 17
 
 def datacenterSymbol (symbol):
   # Validates a DataCite datacenter symbol, per DataCite rules.
-  if not datacenterSymbolRE.match(symbol):
+  if not datacenterSymbolRE.match(symbol) or symbol[-1] == "\n":
     raise django.core.exceptions.ValidationError("Invalid datacenter symbol.")
+
+# EZID borrows its resource type vocabulary from DataCite, and extends
+# that vocabulary by allowing a "specific type" (in DataCite parlance)
+# to follow a "general type" (or type proper) separated by a slash, as
+# in "Image/Photograph".  The following dictionary lists the allowable
+# resource types (these are from version 3.1 of the DataCite Metadata
+# Schema <http://schema.datacite.org/meta/kernel-3/>) and maps them to
+# single-character mnemonic codes for database storage purposes.
+
+resourceTypes = {
+  "Audiovisual": "A",
+  "Collection": "C",
+  "Dataset": "D",
+  "Event": "E",
+  "Image": "I",
+  "InteractiveResource": "N",
+  "Model": "M",
+  "PhysicalObject": "P",
+  "Service": "V",
+  "Software": "S",
+  "Sound": "U",
+  "Text": "T",
+  "Workflow": "W",
+  "Other": "O"
+}
+
+def resourceType (descriptor):
+  # Validates a resource type that is possibly extended with a
+  # specific type as described above.  Returns a pair
+  # (mnemonicCode, normalizedDescriptor).
+  descriptor = descriptor.strip()
+  if "/" in descriptor:
+    gt, st = descriptor.split("/", 1)
+    gt = gt.strip()
+    st = st.strip()
+  else:
+    gt = descriptor
+    st = ""
+  if gt not in resourceTypes:
+    raise django.core.exceptions.ValidationError("Invalid resource type.")
+  return (resourceTypes[gt], gt+"/"+st if st != "" else gt)
+
+goodCrossrefStatusRE = re.compile("(awaiting status change to public|" +\
+  "registration in progress|successfully registered)$")
+badCrossrefStatusRE = re.compile("(registered with warning|" +\
+  "registration failure) \| [^ ]")
+
+def crossrefStatusOrEmpty (value):
+  # Validates that a string is either empty or a CrossRef status.
+  value = value.strip()
+  if value != "":
+    if not goodCrossrefStatusRE.match(value) and\
+      not badCrossrefStatusRE.match(value):
+      raise django.core.exceptions.ValidationError(
+        "Malformed CrossRef status.")
