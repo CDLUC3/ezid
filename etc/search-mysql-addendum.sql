20793abb89a61b93215861f92a5a67863486b225
Greg Janee <gjanee@ucop.edu>
2015-10-31 11:03:34 -0700

    Changed the collation of all columns holding identifiers to be case-sensitive.

--- etc/search-mysql-addendum.sql
+++ etc/search-mysql-addendum.sql
@@ -1,3 +1,18 @@
+-- We assume that the database's default collation is case-insensitive
+-- (e.g., utf8_general_ci).  This is desirable for ordering purposes,
+-- and it will also force usernames and such to be case-insensitively
+-- unique.  But identifiers are case-sensitive.  (For some bizarre
+-- reason the "NOT NULL" has to be re-asserted.)
+
+ALTER TABLE ezidapp_searchgroup MODIFY pid VARCHAR(255) NOT NULL
+  COLLATE 'utf8_bin';
+ALTER TABLE ezidapp_searchuser MODIFY pid VARCHAR(255) NOT NULL
+  COLLATE 'utf8_bin';
+ALTER TABLE ezidapp_searchidentifier MODIFY identifier VARCHAR(255)
+  NOT NULL COLLATE 'utf8_bin';
+
+-- Create the FULLTEXT indexes...
+
 CREATE FULLTEXT INDEX ezidapp_searchidentifier_resourceTitle
   ON ezidapp_searchidentifier (resourceTitle);
 CREATE FULLTEXT INDEX ezidapp_searchidentifier_resourceCreator
