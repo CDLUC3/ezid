b6483b0973bf251af5f28371a7d86c559fc8657e
Andy Mardesich <andy.mardesich@ucop.edu>
2015-11-04 13:46:46 -0800

    Contact Page - error messages

--- code/form_objects.py
+++ code/form_objects.py
@@ -16,14 +16,18 @@ class ContactForm(forms.Form):
     ("newsletter", _("I'd like to sign up for the EZID email newsletter")),
     ("other", _("Other")),
   )
-  contact_reason = forms.ChoiceField(choices=CONTACT_REASONS, 
+  contact_reason = forms.ChoiceField(required=False, choices=CONTACT_REASONS, 
     label = _("Reason for contacting EZID"))
-  your_name = forms.CharField(max_length=200, label=_("Your Name"))
-  email = forms.EmailField(max_length=200, label=_("Your Email"))
+  your_name = forms.CharField(max_length=200, label=_("Your Name"),
+    error_messages={'required': _('Please fill in your name')})
+  email = forms.EmailField(max_length=200, label=_("Your Email"),
+    error_messages={'required': _('Please fill in your email.'),
+                    'invalid': _('Please fill in a valid email address.')})
   affiliation = forms.CharField(required=False, label=_("Your Institution"), 
     max_length=200)
   comment = forms.CharField(label=_("Please indicate any question or comment \
-    you may have"), widget=forms.Textarea(attrs={'rows': '4'}))
+    you may have"), widget=forms.Textarea(attrs={'rows': '4'}),
+    error_messages={'required': _('Please fill in a question or comment.')})
 
   # Translators: These options appear in drop-down on contact page
   REFERRAL_SOURCES = (
@@ -34,5 +38,5 @@ class ContactForm(forms.Form):
     ("webinar", _("Webinar")),
     ("other", _("Other")),
   )
-  hear_about = forms.ChoiceField(choices=REFERRAL_SOURCES,
+  hear_about = forms.ChoiceField(required=False, choices=REFERRAL_SOURCES,
     label=_("How did you hear about us?"))
