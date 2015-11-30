41d505b9cd7405e10ca439b8694104b6dc2e0bf6
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-11-30 15:48:21 -0800

    select mobile menu with icon via jquery and show/hide

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -1,1 +1,11 @@
 // ##### Main JavaScript ##### //
+
+// Show/hide mobile menu via mobile menu icon:
+
+$(document).ready(function(){
+  // Change initial 'header__nav--selected' class to 'header__nav' (set to 'display: none') before toggling below
+  $('#js-header__mobile-menu').attr('class', 'header__nav');
+  $('#js-header__mobile-menu-icon').click(function(){
+    $('#js-header__mobile-menu').toggleClass('header__nav header__nav--selected');
+  });
+});
