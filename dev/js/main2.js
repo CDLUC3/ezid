20b8b634540327ff4183dfbb913fa3bb8e5ac7ca
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-12-02 14:50:47 -0800

    replace header menu link with button; remove old js

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
   
   // ***** Show/hide mobile menu via mobile menu icon ***** //
 
-  // Before toggling menu, change default header menu class to non-selected state and change default aria attributes:
+  // Before toggling menu, change default header menu class to non-selected state and change default aria attribute:
 
   $('#js-header__mobile-menu').attr('class', 'header__nav');
   $('#js-header__mobile-menu').attr('aria-expanded', 'false');
-  $('#js-header__mobile-menu-icon').attr('aria-pressed', 'false');
 
   // Toggle classes and attributes:
   $('#js-header__mobile-menu-icon').click(function(){
@@ -19,12 +18,6 @@ $(document).ready(function(){
       $('#js-header__mobile-menu').attr('aria-expanded', 'true');
     } else {
       $('#js-header__mobile-menu').attr('aria-expanded', 'false');
-    }
-
-    if($('#js-header__mobile-menu-icon').attr('aria-pressed') == 'false') {
-      $('#js-header__mobile-menu-icon').attr('aria-pressed', 'true');
-    } else {
-      $('#js-header__mobile-menu-icon').attr('aria-pressed', 'false');
     }
 
   });
