af6516af13deaa195d692a8a2ea27aad270d5e00
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-12-02 12:11:47 -0800

    add js to toggle header menu aria-expanded attribute

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -1,11 +1,22 @@
 // ##### Main JavaScript ##### //
 
-// Show/hide mobile menu via mobile menu icon:
-
 $(document).ready(function(){
-  // Change initial 'header__nav--selected' class to 'header__nav' (set to 'display: none') before toggling below
-  $('#js-header__mobile-menu').attr('class', 'header__nav');
+  
+  // ***** Show/hide mobile menu via mobile menu icon ***** //
+
+  // Before toggling menu, change initial 'header__nav--selected' class to 'header__nav' and add aria-expanded attribute:
+  $('#js-header__mobile-menu').attr({'class': 'header__nav', 'aria-expanded': 'false'});
+
+  // Toggle classes and attribute:
   $('#js-header__mobile-menu-icon').click(function(){
+    
     $('#js-header__mobile-menu').toggleClass('header__nav header__nav--selected', 300, 'easeInOutCubic');
+
+    if($('#js-header__mobile-menu').attr('aria-expanded') == 'false') {
+      $('#js-header__mobile-menu').attr('aria-expanded', 'true');
+    } else {
+      $('#js-header__mobile-menu').attr('aria-expanded', 'false');
+    }
+
   });
 });
