53e191f6d3ad03b6ba6dfaad9853e7db1bef3918
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-12-02 15:02:03 -0800

    rename header menu ids and classes

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -6,18 +6,18 @@ $(document).ready(function(){
 
   // Before toggling menu, change default header menu class to non-selected state and change default aria attribute:
 
-  $('#js-header__mobile-menu').attr('class', 'header__nav');
-  $('#js-header__mobile-menu').attr('aria-expanded', 'false');
+  $('#js-header__nav').attr('class', 'header__nav');
+  $('#js-header__nav').attr('aria-expanded', 'false');
 
   // Toggle classes and attributes:
-  $('#js-header__mobile-menu-icon').click(function(){
+  $('#js-header__nav-button').click(function(){
     
-    $('#js-header__mobile-menu').toggleClass('header__nav header__nav--selected', 300, 'easeInOutCubic');
+    $('#js-header__nav').toggleClass('header__nav header__nav--selected', 300, 'easeInOutCubic');
 
-    if($('#js-header__mobile-menu').attr('aria-expanded') == 'false') {
-      $('#js-header__mobile-menu').attr('aria-expanded', 'true');
+    if($('#js-header__nav').attr('aria-expanded') == 'false') {
+      $('#js-header__nav').attr('aria-expanded', 'true');
     } else {
-      $('#js-header__mobile-menu').attr('aria-expanded', 'false');
+      $('#js-header__nav').attr('aria-expanded', 'false');
     }
 
   });
