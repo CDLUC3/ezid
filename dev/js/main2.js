afb3ae15fc6599afdddc153b93a47fbb317bec02
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-12-02 16:31:14 -0800

    replace aria-expanded with aria-hidden in header nav

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -7,17 +7,17 @@ $(document).ready(function(){
   // Before toggling menu, change default header menu class to non-selected state and change default aria attribute:
 
   $('#js-header__nav').attr('class', 'header__nav');
-  $('#js-header__nav').attr('aria-expanded', 'false');
+  $('#js-header__nav').attr('aria-hidden', 'true');
 
   // Toggle classes and attributes:
   $('#js-header__nav-button').click(function(){
     
     $('#js-header__nav').toggleClass('header__nav header__nav--selected', 300, 'easeInOutCubic');
 
-    if($('#js-header__nav').attr('aria-expanded') == 'false') {
-      $('#js-header__nav').attr('aria-expanded', 'true');
+    if($('#js-header__nav').attr('aria-hidden') == 'true') {
+      $('#js-header__nav').attr('aria-hidden', 'false');
     } else {
-      $('#js-header__nav').attr('aria-expanded', 'false');
+      $('#js-header__nav').attr('aria-hidden', 'true');
     }
 
   });
