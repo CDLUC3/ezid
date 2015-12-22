ed8a4d3c5f656bcbbb7d796f05e871809553fc2c
Joel Hagedorn <joel.hagedorn@ucop.edu>
2015-12-21 16:19:50 -0800

    add jquery to add required class to required text fields

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -45,4 +45,16 @@ $(document).ready(function(){
 
   });
 
+  // ***** HTML Form Validation ***** //
+
+  // If 'required' attribute exists on a text input, add 'Required' class to its label:
+
+  if ($('.fcontrol__text-field-stacked').is('[required]')) {
+    $('.fcontrol__text-label-stacked').addClass('fcontrol__label-required');
+  }
+
+  if ($('.fcontrol__text-field-inline').is('[required]')) {
+    $('.fcontrol__text-label-inline').addClass('fcontrol__label-required');
+  }
+
 }); // Close $(document).ready(function()
