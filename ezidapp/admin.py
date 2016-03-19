4ad996a315547dbd8a429a60577ec63aba53fa80
Greg Janee <gjanee@ucop.edu>
2016-03-19 10:29:23 -0700

    Admin configuration for new account worksheets.

--- ezidapp/admin.py
+++ ezidapp/admin.py
@@ -17,8 +17,10 @@
 #
 # -----------------------------------------------------------------------------
 
+import django.conf
 import django.contrib.admin
 import django.contrib.messages
+import django.core.mail
 import django.core.urlresolvers
 import django.core.validators
 import django.db.models
@@ -27,6 +29,18 @@ import django.forms
 import config
 import models
 import ui_common
+
+_ezidUrl = None
+_newAccountEmailAddresses = None
+
+def _loadConfig ():
+  global _ezidUrl, _newAccountEmailAddresses
+  _ezidUrl = config.get("DEFAULT.ezid_base_url")
+  _newAccountEmailAddresses = [a for a in\
+    config.get("email.new_account_email").split(",") if len(a) > 0]
+
+_loadConfig()
+config.registerReloadListener(_loadConfig)
 
 class SuperuserSite (django.contrib.admin.sites.AdminSite):
   # This administrative site allows full access.
@@ -173,3 +187,67 @@ class StoreDatacenterAdmin (django.contrib.admin.ModelAdmin):
     return False
 
 superuser.register(models.StoreDatacenter, StoreDatacenterAdmin)
+
+class NewAccountWorksheetForm (django.forms.ModelForm):
+  def __init__ (self, *args, **kwargs):
+    super(NewAccountWorksheetForm, self).__init__(*args, **kwargs)
+    self.fields["orgStreetAddress"].widget =\
+      django.contrib.admin.widgets.AdminTextareaWidget()
+
+class NewAccountWorksheetAdmin (django.contrib.admin.ModelAdmin):
+  def organizationName (self, obj):
+    return obj.orgName
+  organizationName.short_description = "organization name"
+  search_fields = ["orgName", "orgAcronym", "orgStreetAddress", "reqName",
+    "priName", "secName", "reqUsername", "reqComments", "setGroupname",
+    "setUsername", "setNotes"]
+  actions = None
+  list_filter = ["staReady", "staShouldersCreated", "staAccountCreated"]
+  ordering = ["-requestDate", "orgName"]
+  list_display = ["organizationName", "requestDate"]
+  fieldsets = [
+    (None, { "fields": ["requestDate"] }),
+    ("Organization", { "fields": ["orgName", "orgAcronym", "orgUrl",
+      "orgStreetAddress"] }),
+    ("Requestor", { "fields": ["reqName", "reqEmail", "reqPhone"] }),
+    ("Primary contact", { "fields": [("priName", "priUseRequestor"),
+      "priEmail", "priPhone"] }),
+    ("Secondary contact (optional)", { "fields": ["secName", "secEmail",
+      "secPhone"] }),
+    ("Request", { "fields": ["reqUsername", ("reqAccountEmail",
+      "reqAccountEmailUsePrimary"), ("reqArks", "reqDois"), "reqShoulders",
+      "reqCrossref", ("reqCrossrefEmail", "reqCrossrefEmailUseAccount"),
+      "reqHasExistingIdentifiers", "reqComments"] }),
+    ("Setup", { "fields": ["setRealm", ("setGroupname", "setExistingGroup"),
+      ("setUsername", "setUsernameUseRequested"), "setNeedShoulders",
+      "setNeedMinters", "setNotes"] }),
+    ("Status", { "fields": ["staReady", "staShouldersCreated",
+      "staAccountCreated"] })]
+  form = NewAccountWorksheetForm
+  def save_model (self, request, obj, form, change):
+    obj.save()
+    newStatus = []
+    for f in ["staReady", "staShouldersCreated", "staAccountCreated"]:
+      if f in form.changed_data and getattr(obj, f):
+        newStatus.append(obj._meta.get_field(f).verbose_name)
+    if len(newStatus) > 0:
+      m = ("The status of a new account request has changed.\n\n" +\
+        "Organization: %s\n" +\
+        "Request date: %s\n" +\
+        "New status: %s\n\n" +
+        "View the account's worksheet at:\n\n" +\
+        "%s%s\n\n" +\
+        "This is an automated email.  Please do not reply.") %\
+        (obj.orgName, str(obj.requestDate), ", ".join(newStatus),
+        _ezidUrl, django.core.urlresolvers.reverse(
+        "admin:ezidapp_newaccountworksheet_change", args=[obj.id]))
+      try:
+        django.core.mail.send_mail("New account request status change", m,
+          django.conf.settings.SERVER_EMAIL, _newAccountEmailAddresses)
+      except Exception, e:
+        django.contrib.messages.error(request,
+          "Error sending status change email: " + str(e))
+      else:
+        django.contrib.messages.success(request, "Status change email sent.")
+
+superuser.register(models.NewAccountWorksheet, NewAccountWorksheetAdmin)
