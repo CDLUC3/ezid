a5377778b5a162a66b6e8856bae5f3445d0b436a
Greg Janee <gjanee@ucop.edu>
2016-04-18 20:43:39 -0700

    Function renames.

--- ezidapp/management/commands/ezidadminsetpassword.py
+++ ezidapp/management/commands/ezidadminsetpassword.py
@@ -8,7 +8,7 @@ execfile(os.path.join(os.path.dirname(os.path.dirname(
   os.path.dirname(os.path.dirname(__file__)))), "tools", "offline.py"))
 
 import config
-import ezidapp.models.store_user
+import ezidapp.models
 
 class Command (django.core.management.base.BaseCommand):
   help = "Set the EZID administrator password"
@@ -20,6 +20,6 @@ class Command (django.core.management.base.BaseCommand):
         .exists():
         django.contrib.auth.models.User.objects.create_superuser(
           username=u, password=None, email="")
-      o = ezidapp.models.store_user.getByUsername(u)
+      o = ezidapp.models.getUserByUsername(u)
       o.setPassword(p)
       o.save()
