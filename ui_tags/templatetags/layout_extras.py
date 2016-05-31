83b1c8b0eaac3bdbceb6aae6bfad862d30508c66
Andy Mardesich <andy.mardesich@ucop.edu>
2016-05-31 15:50:54 -0700

    WCAG: Help popover; Login/out

--- ui_tags/templatetags/layout_extras.py
+++ ui_tags/templatetags/layout_extras.py
@@ -83,7 +83,8 @@ def active_id_display(id_text, testPrefixes):
 def help_icon(id_of_help, specifics="", css_class="button__icon-help"):
   title = _("Click for additional help") + " " + unicode(specifics)
   return '<a id="' + str(id_of_help) + '" role="button" data-toggle="popover"' +\
-    'data-trigger="click">' + \
+    'data-placement="auto bottom" data-trigger="click" aria-describedby="' +\
+    str(id_of_help) + '_content" tabindex="0">' + \
     '<img src="/static/images/iconHelp.svg" alt="' + str(title) + '"' + \
     ' class="' + str(css_class) + '" title="' + str(title) + '"/></a>'
 
