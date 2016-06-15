5c9ba867523f753eb26b6d943fe6394527ee1bbe
Greg Janee <gjanee@ucop.edu>
2016-06-15 13:46:40 -0700

    Cleanup.

--- settings/development.py
+++ settings/development.py
@@ -5,9 +5,8 @@ DEPLOYMENT_LEVEL = "development"
 ALLOWED_HOSTS = ["ias-ezid-dev.cdlib.org", "ezid-dev-alt-aws.cdlib.org"]
 
 # Select one localization below to be triggered by the alt hostname:
-# ToDo: Change this back to gjanee@ucop.edu after demo
-LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] = ("purdue", ["datacite@purdue.edu"])
-# LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] =\
-#  ("jisc-edina", ["gjanee@ucop.edu"])
+# LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] = ("purdue", ["gjanee@ucop.edu"])
+LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] =\
+  ("jisc-edina", ["gjanee@ucop.edu"])
 
 injectSecrets(DEPLOYMENT_LEVEL)
