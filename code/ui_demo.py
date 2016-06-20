e9205ccc0303d4f2a36858dae91cb2433148c66d
Andy Mardesich <andy.mardesich@ucop.edu>
2016-06-20 13:57:07 -0700

    id_gen_result checks

--- code/ui_demo.py
+++ code/ui_demo.py
@@ -2,22 +2,22 @@ import ui_common as uic
 from django.shortcuts import redirect
 import ui_create
 import urllib
+
 def index(request):
   d = { 'menu_item' : 'ui_home.learn' }
   return redirect("ui_demo.simple")
-  return uic.render(request, 'create/index', d)
 
 def simple(request):
   d = { 'menu_item' :'ui_home.learn' }
   d["testPrefixes"] = uic.testPrefixes
   d['prefixes'] = sorted(uic.testPrefixes, key=lambda p: p['namespace'].lower()) #must be done before calling form processing
   d = ui_create.simple_form(request, d)
-  result = d['id_gen_result']
+  result = 'edit_page' if 'id_gen_result' not in d else d['id_gen_result']
   if result == 'edit_page':
     return uic.render(request, 'demo/simple', d)  # ID Creation page 
-  elif result == 'bad_request':
+  elif d['id_gen_result'] == 'bad_request':
     return uic.badRequest(request)
-  elif result.startswith('created_identifier:'):
+  elif d['id_gen_result'].startswith('created_identifier:'):
     return redirect("/id/" + urllib.quote(result.split()[1], ":/"))   # ID Details page
 
 def advanced(request):
@@ -25,7 +25,7 @@ def advanced(request):
   d["testPrefixes"] = uic.testPrefixes
   d['prefixes'] = sorted(uic.testPrefixes, key=lambda p: p['namespace'].lower()) #must be done before calling form processing
   d = ui_create.adv_form(request, d)
-  result = d['id_gen_result']
+  result = 'edit_page' if 'id_gen_result' not in d else d['id_gen_result']
   if result == 'edit_page':
     return uic.render(request, 'demo/advanced', d)  # ID Creation page 
   elif result == 'bad_request':
