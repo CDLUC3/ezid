325baa98ff03a7942dc659e0d2191441cf8494b1
Andy Mardesich <andy.mardesich@ucop.edu>
2016-08-16 12:01:46 -0700

    Adv ID Create: set an anchor tag when selecting profile; Include remainder in request; Removed focus on first form element from all pages

--- static/javascripts/advanced_create.js
+++ static/javascripts/advanced_create.js
@@ -1,21 +1,41 @@
-/* include file to attach javascript actions to simple creation form */
-
+/* JavaScript actions for advanced ID creation form */
 $(document).ready(function() {
-  /* Determine value of 'current_profile' based on shoulder that is selected  */
-  var orig_val = $('input[name=shoulder]:checked', '#create_form').val();
-	
-  function do_get(){
-      var frm = $('#create_form');
-      frm.attr('action', location.pathname + '?publish=' + $("[name='publish']").val());
-      frm.unbind('submit');
-      frm.attr('method', 'get');
-      frm.submit();
-  }
 
+// ***** Navigational Tabs Simple <-> Advanced ***** //
+
+  /* Redirect to Simple ID create: Handle tab clicking for keyboard users */
+  function actTab1() {
+    document.getElementById('tab-1').checked = false;
+    document.getElementById('tab-2').checked = true;
+    var action = location.pathname.split("/")[1];  // Handle create or demo pages
+    window.location.href = "/" + action + "/simple";
+  }
+  // Act on keyboard enter or spacebar
+  $("#tab-1-label").keyup(function(e){
+    var code = e.which;
+    if(code==13)e.preventDefault();
+    if(code==32||code==13||code==188||code==186){
+      actTab1();
+    }
+  });
+
+  /* ToDo: Bring in both simple and advanced content onto the same page
+     Then this JavaScript will be unnecessary since tabs will swap quickly
+     btwn simple/advanced content */
+  $('#tab-1:not(:checked)').on('change', function() {
+    actTab1();
+  });
+
+
+// ***** Shoulder Selection ***** //
+
+  /* Determine value of 'current_profile' based on shoulder that is selected  */
+  var orig_scheme = $('input[name=shoulder]:checked', '#create_form').val();
+	
   // submit form when shoulder changes 
   $("input[name=shoulder]").change(function(e) {
       var new_scheme = e.target.value.split(':')[0];
-      if(orig_val.split(':')[0] != new_scheme){
+      if(orig_scheme.split(':')[0] != new_scheme){
         // changing scheme
           if(new_scheme == 'doi'){
               $('#current_profile').val('datacite');
@@ -24,8 +44,17 @@ $(document).ready(function() {
           }
           do_get();
       }
-      orig_val = e.target.value;
+      orig_scheme = e.target.value;
   });
+
+
+// ***** Publish/Reserved Selection ***** //
+
+  // When publish button is selected, hide or reveal harvesting selector
+  $("[name='publish']").bind("change", function(){
+      setHarvestingHidden();
+  });
+  setHarvestingHidden();
 
   function setHarvestingHidden(){
       if ($('#publish_true').is(':checked')) {
@@ -37,15 +66,12 @@ $(document).ready(function() {
       }
   }
 
-  // When publish button is selected, hide or reveal harvesting selector
-  $("[name='publish']").bind("change", function(){
-      setHarvestingHidden();
-  });
-  setHarvestingHidden();
+// ***** Profile Selection ***** //
 
   // submit form when profile changes
   $("#current_profile").bind("change", function(event){
-      do_get();
+      var includeAnchor = true;
+      do_get(includeAnchor);
   });
 
   // When user submits: 
@@ -68,26 +94,21 @@ $(document).ready(function() {
       $('#create_form').submit();
   });
 
-  /* Redirect to Simple ID create: Handle tab clicking for keyboard users */
-  function actTab1() {
-    document.getElementById('tab-1').checked = false;
-    document.getElementById('tab-2').checked = true;
-    action = location.pathname.split("/")[1];  // Handle create or demo pages
-    window.location.href = "/" + action + "/simple";
+
+// ***** Submit form when profile or shoulder changes ******* //
+
+  // If triggered by profile, then, after submission, scroll page down to specified anchor 
+  function do_get(includeAnchor=false){
+      var frm = $('#create_form');
+      frm.attr('action', location.pathname + '?publish=' +
+        $("[name='publish']").val() + '&remainder=' + $("#remainder").val());
+      if (includeAnchor) {
+        var input = $("<input>", { type: "hidden", name: "anchor", value: "create__publish" });
+        frm.append($(input));
+      }
+      frm.unbind('submit');
+      frm.attr('method', 'get');
+      frm.submit();
   }
-  // Act on keyboard enter or spacebar
-  $("#tab-1-label").keyup(function(e){
-    var code = e.which;
-    if(code==13)e.preventDefault();
-    if(code==32||code==13||code==188||code==186){
-      actTab1();
-    }
-  });
-/* ToDo: Bring in both simple and advanced content onto the same page
-   Then this JavaScript will be unnecessary since tabs will swap quickly
-   btwn simple/advanced content */
-  $('#tab-1:not(:checked)').on('change', function() {
-    actTab1();
-  });
 
 });
