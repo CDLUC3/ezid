fa18754cfab06676058cc27b4609372cc34a47f1
Andy Mardesich <andy.mardesich@ucop.edu>
2016-08-17 10:02:21 -0700

    Moved GA keyword specific params out of static script; Added object_type to concatenation

--- static/javascripts/google_analytics_searchconcat.js
+++ static/javascripts/google_analytics_searchconcat.js
@@ -1,4 +1,4 @@
-// Google Analytics does NOT capture all search parameters in URL
+// Google Analytics (GA) does NOT capture all search parameters in URL
 // https://www.en.advertisercommunity.com/t5/Reports/Multiple-search-category-parameters-on-the-same-URL/m-p/567891/highlight/true#M5801
 // This script concatenates all values that have been assigned to an expected set of possibleKeys. 
 // Concatenated result of this search
@@ -8,38 +8,38 @@
 
 var GA_SEARCHPARMS_LIB = GA_SEARCHPARMS_LIB || (function(){
 
-  var _args = {};
+  var _uri = '', _mainKey = '', _allKeys = []
   return {
-    init : function(Args) {
-      // expecting url parameters 
-      // eg: "keywords=&creator=Dejaco&title=NewHybrids"
-      _args = Args
+    init : function(uri, mainKey, allKeys) {
+      _uri = uri           // expecting url parameters 
+                           // eg: "keywords=&creator=Dejaco&title=NewHybrids"
+      _mainKey = mainKey   // GA single query parameter
+      _allKeys = allKeys   // Array of all query parameters you want tracked in GA
     },
     concat_parms : function() { 
-      var keywordsFieldUsed = false,
-        possibleKeys = ['keywords', 'title', 'creator', 'publisher'],
+      var mainFieldUsed = false,
         params = [],
-        keywordValue = '',
-        out = 'keywords=';
-      if(typeof _args !== "undefined" && _args.includes("=")){
-        var p = _args.split('&');
+        mainKeyValue = '',
+        out = _mainKey + '=';
+      if (_uri && _uri.includes("=") && _mainKey && _allKeys && _allKeys.length > 0){
+        var p = _uri.split('&');
         for (var i = 0; i < p.length; i++) {
           var pair = p[i].split('=')
           if (pair[1] !== '') {
             // remove any leading question mark
             k = pair[0].replace(/^\?/g, '')
-            if (possibleKeys.indexOf(k) < 0) {
+            if (_allKeys.indexOf(k) < 0) {
               continue
             }
-            if (k == 'keywords' && !keywordsFieldUsed) {
-              keywordsFieldUsed = true
-              keywordValue = pair[1] 
+            if (k == _mainKey && !mainFieldUsed) {
+              mainFieldUsed = true
+              mainKeyValue = pair[1] 
             } else {
               params.push(pair[0] + ':' + pair[1])
             }
           }
         }
-        out += keywordValue
+        out += mainKeyValue
         if (params.length > 0) {
           out += "," + params.join(',')
         }
