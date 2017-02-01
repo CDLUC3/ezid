ad4e68a90fd31822c7cbf3d1a20b1b990b32bad3
Greg Janee <gjanee@ucop.edu>
2017-01-31 19:16:01 -0800

    Added support for GeoJSON.

--- code/geometry_util.py
+++ code/geometry_util.py
@@ -13,6 +13,7 @@
 #
 # -----------------------------------------------------------------------------
 
+import json
 import lxml.etree
 import re
 
@@ -78,6 +79,78 @@ def kmlPolygonToDatacite (kml):
     lxml.etree.SubElement(p, _q("pointLongitude")).text = c[0]
     lxml.etree.SubElement(p, _q("pointLatitude")).text = c[1]
     if len(c) == 3 and float(c[2]) != 0: altitudeWarning = True
+  if len(output) < 4:
+    return "polygon has insufficient coordinate tuples (4 required)"
+  if float(output[0][0].text) != float(output[-1][0].text) or\
+    float(output[0][1].text) != float(output[-1][1].text):
+    return "polygon first coordinate does not match last"
+  warnings = []
+  if innerBoundaryWarning: warnings.append("polygon inner boundaries ignored")
+  if altitudeWarning: warnings.append("altitude coordinates ignored")
+  return (output, warnings)
+
+_geojsonTypes = ["Point", "MultiPoint", "LineString", "MultiLineString",
+  "Polygon", "MultiPolygon", "GeometryCollection"]
+
+def _isNestedList (o, n):
+  if type(o) is not list: return False
+  if n > 1:
+    if len(o) == 0: return False
+    if not all(_isNestedList(m, n-1) for m in o): return False
+  return True
+
+def geojsonPolygonToDatacite (geojson):
+  """
+  Converts a polygon defined in a GeoJSON <RFC 7946,
+  https://tools.ietf.org/html/rfc7946> document to a DataCite 4.0
+  <geoLocationPolygon> element.  The return is a pair
+  (lxml.etree.Element, [warning, ...]) if successful or a string error
+  message if not.  The conversion fails for the usual reasons
+  (malformed JSON, etc.) but also if the document defines more than
+  one geometry or does not define a polygon.  Polygon holes and
+  non-zero altitude coordinates are ignored and result in warnings.
+  """
+  objects = []
+  def objectHandler (d):
+    if d.get("type", "unknown") in _geojsonTypes and\
+      d["type"] != "GeometryCollection":
+      objects.append(d)
+    return d
+  try:
+    root = json.loads(geojson, object_hook=objectHandler)
+  except Exception, e:
+    return "JSON parse error: " + util.formatException(e)
+  if type(root) is not dict or\
+    root.get("type", "unknown") not in _geojsonTypes:
+    return "not a GeoJSON document"
+  if len(objects) > 1: return "document defines more than one geometry"
+  if len(objects) == 0 or\
+    objects[0]["type"] not in ["Polygon", "MultiPolygon"]:
+    return "no polygon found"
+  p = objects[0]
+  if not _isNestedList(p.get("coordinates", {}),
+    3 if p["type"] == "Polygon" else 4):
+    return "malformed GeoJSON document"
+  if p["type"] == "MultiPolygon":
+    if len(p["coordinates"]) > 1:
+      return "document defines more than one geometry"
+    p["coordinates"] = p["coordinates"][0]
+  innerBoundaryWarning = (len(p["coordinates"]) > 1)
+  output = lxml.etree.Element(_q("geoLocationPolygon"),
+    nsmap={ None: _dataciteNamespace })
+  altitudeWarning = False
+  for ct in p["coordinates"][0]:
+    if len(ct) not in [2, 3] or not isinstance(ct[0], (int, float)) or\
+      not isinstance(ct[1], (int, float)) or\
+      (len(ct) == 3 and not isinstance(ct[2], (int, float))):
+      return "malformed coordinates"
+    if float(ct[0]) < -180 or float(ct[0]) > 180 or\
+      float(ct[1]) < -90 or float(ct[1]) > 90:
+      return "coordinates out of range"
+    p = lxml.etree.SubElement(output, _q("polygonPoint"))
+    lxml.etree.SubElement(p, _q("pointLongitude")).text = str(ct[0])
+    lxml.etree.SubElement(p, _q("pointLatitude")).text = str(ct[1])
+    if len(ct) == 3 and float(ct[2]) != 0: altitudeWarning = True
   if len(output) < 4:
     return "polygon has insufficient coordinate tuples (4 required)"
   if float(output[0][0].text) != float(output[-1][0].text) or\
