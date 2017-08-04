139de35bb74c734ff37568cb6de9f14ad0c29280
Andy Mardesich <andy.mardesich@ucop.edu>
2017-08-04 14:18:36 -0700

    Typo

--- settings/development.py
+++ settings/development.py
@@ -7,6 +7,6 @@ ALLOWED_HOSTS = ["ids-ezid-dev.cdlib.org", "ezid-dev-alt-aws.cdlib.org"]
 # Select one localization below to be triggered by the alt hostname:
 LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] = ("purdue", ["gjanee@ucop.edu"])
 # LOCALIZATIONS["ezid-dev-alt-aws.cdlib.org"] =\
-  ("jisc-edina", ["gjanee@ucop.edu"])
+#  ("jisc-edina", ["gjanee@ucop.edu"])
 
 injectSecrets(DEPLOYMENT_LEVEL)
