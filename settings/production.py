a909ba050f6003e68c8d7c1faec87aa5a81c6a88
Greg Janee <gjanee@ucop.edu>
2019-03-08 09:12:21 -0800

    Added Maria to the appropriate email lists.

--- settings/production.py
+++ settings/production.py
@@ -6,6 +6,7 @@ DEBUG = False
 
 ADMINS.append(("Marisa Strong", "marisa.strong@ucop.edu"))
 ADMINS.append(("Perry Willett", "perry.willett@ucop.edu"))
+ADMINS.append(("Maria Gould", "maria.gould@ucop.edu"))
 
 ALLOWED_HOSTS = ["ezid.cdlib.org", "ezid.lib.purdue.edu"]
 LOCALIZATIONS["ezid.lib.purdue.edu"] = ("purdue", ["datacite@purdue.edu"])
