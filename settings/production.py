ae6fa1ef639d4e91a8b74fd084fdb1ff6d551f34
Greg Janee <gjanee@ucop.edu>
2019-06-21 10:46:54 -0700

    Removed Perry from email notifications.

--- settings/production.py
+++ settings/production.py
@@ -5,7 +5,6 @@ DEPLOYMENT_LEVEL = "production"
 DEBUG = False
 
 ADMINS.append(("Marisa Strong", "marisa.strong@ucop.edu"))
-ADMINS.append(("Perry Willett", "perry.willett@ucop.edu"))
 ADMINS.append(("Maria Gould", "maria.gould@ucop.edu"))
 
 ALLOWED_HOSTS = ["ezid.cdlib.org"]
