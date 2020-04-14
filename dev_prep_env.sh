45d332e2c09303aedfcdc748527fbd3e1fe75387
Roger Dahl <git@dahlsys.com>
2020-04-14 14:16:07 -0600

    Prepare paths for running the EZID app

---
+++ dev_prep_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Create some paths that are assumed to be present by the EZID app.
+
+# Run from ./ezid/
+
+mkdir -p ../logs
+mkdir -p ../download/public
+
+touch ../logs/transaction_log
