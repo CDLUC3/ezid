5ff65c43fd06d55c569bf848d4f82158318ece3f
Dave <datadavev@users.noreply.github.com>
2020-05-26 13:56:57 -0400

    Oops, typo

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -2,7 +2,7 @@ TESTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd ${TESTDIR}/test_docs
 for f in $(ls *.bdb); do
   A=${f%_*};
-  B={f##*_};
+  B=${f##*_};
   C=${B%.*};
   D=${HOME}/.minders/${A}/${C};
   mkdir -p ${D};
