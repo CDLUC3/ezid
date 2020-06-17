fb3d6c32dc87fad1aea20d163f1673f0475dadbe
Roger Dahl <git@dahlsys.com>
2020-06-17 14:40:34 -0600

    Temporarily add "EZ" to end of all spings generated with the Python minter

--- tests/test_nog_minter.py
+++ tests/test_nog_minter.py
@@ -5,11 +5,11 @@ import django.conf
 
 import backports.lzma as lzma
 import utils.filesystem
-import code.nog_minter
+import impl.nog_minter
 
 NAAN_STR = "77913"
 PREFIX_STR = "r7"
-MINT_COUNT = 10000
+MINT_COUNT = 1000
 
 
 # noinspection PyClassHasNoInit,PyProtectedMember
@@ -33,15 +33,15 @@ class TestNogMinter:
         with lzma.open(csv_path) as f:
 
             for i, python_sping in enumerate(
-                code.nog_minter.mint(NAAN_STR, PREFIX_STR, MINT_COUNT, dry_run=False)
+                impl.nog_minter.mint(NAAN_STR, PREFIX_STR, MINT_COUNT, dry_run=False)
             ):
                 if i == MINT_COUNT:
                     break
                 perl_sping = "{}/{}{}".format(
                     NAAN_STR, PREFIX_STR, f.readline().strip()
                 )
-                # assert (
-                #     perl_sping == python_sping
-                # ), "Mismatch after {} spings. python={} != perl={}".format(
-                #     i, python_sping, perl_sping
-                # )
+                assert (
+                    perl_sping == python_sping
+                ), "Mismatch after {} spings. python={} != perl={}".format(
+                    i, python_sping, perl_sping
+                )
