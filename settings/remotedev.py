0f7a1633301cef90e0d5c353c21caf0c07540435
rogerdahl <git@dahlsys.com>
2020-09-17 11:36:49 -0600

    Add stg hosts to remotedev.ALLOWED_HOSTS

--- settings/remotedev.py
+++ settings/remotedev.py
@@ -1,5 +1,3 @@
-import os.path
-
 from common import *
 
 DEPLOYMENT_LEVEL = "remotedev"
@@ -14,6 +12,13 @@ SEARCH_MYSQL = True
 # If both the store and search databases are on MySQL, they share a single database.
 SEARCH_STORE_SAME_DATABASE = STORE_MYSQL and SEARCH_MYSQL
 
-ALLOWED_HOSTS = ["uc3-ezidx2-dev.cdlib.org", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    u'ezid-stg.cdlib.org',
+    u'uc3-ezidx2-dev.cdlib.org',
+    u'uc3-ezidui01x2-stg.cdlib.org',
+    u'localhost',
+    u'127.0.0.1',
+    u'172.31.57.125',
+]
 
 injectSecrets(DEPLOYMENT_LEVEL)
