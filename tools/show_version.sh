1f91787df04df10520f9eb92ae2b23f62964819f
datadavev <datadavev@users.noreply.github.com>
2020-10-06 11:46:04 -0400

    Script to emulate Mercurial identify command

---
+++ tools/show_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# emulates "hg identify -inb -R path"
+#
+target_path='.'
+if [ ! -z "${1}" ]; then
+  target_path="${1}"
+fi
+pushd "${target_path}" > /dev/null
+echo "$(git describe --abbrev=12 --always) $(git status --porcelain | wc -l | xargs) $(git branch --show-current)"
+popd > /dev/null
