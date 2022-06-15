319aff7fb319c509c2d07940243abe12f75e2523
datadavev <605409+datadavev@users.noreply.github.com>
2022-06-15 17:09:00 -0400

    Added diagnostic tool for showing current queue statuses

---
+++ ezidapp/management/commands/diag-queue-stats.py
@@ -0,0 +1,54 @@
+import contextlib
+import json
+import logging
+
+import django.apps
+import django.conf
+import django.contrib.auth.models
+import django.core.management
+import django.db.models
+import django.db.transaction
+
+import ezidapp.models.async_queue
+import ezidapp.models.identifier
+
+_L = logging.getLogger(__name__)
+
+class Command(django.core.management.BaseCommand):
+    help = __doc__
+
+    def __init__(self):
+        super(Command, self).__init__()
+
+    def handle(self, *_, **opt):
+        queue_classes = [
+            ("binder", ezidapp.models.async_queue.BinderQueue),
+            ("datacite", ezidapp.models.async_queue.DataciteQueue),
+            ("crossref", ezidapp.models.async_queue.CrossrefQueue),
+            ("searchindexer", ezidapp.models.async_queue.SearchIndexerQueue),
+        ]
+        queue_stats = {
+            'download': {}
+        }
+        #Download queue is a different beast
+        _L.info("Processing queue: download...")
+        res = ezidapp.models.async_queue.DownloadQueue.objects\
+                .all()\
+                .values('stage')\
+                .annotate(total=django.db.models.Count('stage'))\
+                .order_by()
+        for row in res:
+            queue_stats['download'][row['stage']] = row['total']
+
+        for q_class in queue_classes:
+            q_name = q_class[0]
+            _L.info(f"Processing queue: {q_name}")
+            res = q_class[1].objects\
+                .all()\
+                .values('status')\
+                .annotate(total=django.db.models.Count('status'))\
+                .order_by()
+            queue_stats[q_name] = {}
+            for row in res:
+                queue_stats[q_name][row['status']] = row['total']
+        print(json.dumps(queue_stats, indent=2))
