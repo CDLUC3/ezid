6f3a63ecd085ba5626945310225536349aafed4e
datadavev <605409+datadavev@users.noreply.github.com>
2022-06-22 16:58:01 -0400

    added reminder to flesh out this test case

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -8,7 +8,7 @@ import pytest
 
 import ezidapp.models.validation
 
-#@pytest.mark.django_db
+#TODO: Flesh out the test cases to match all the possibilities in the tested method
 @pytest.mark.parametrize("test,expected",[
     ('1961', '1961'),
     ('196104','1961-04'),
@@ -17,3 +17,4 @@ import ezidapp.models.validation
 def test_publicationDate(test, expected):
     res = ezidapp.models.validation.publicationDate(test)
     assert res == expected
+
