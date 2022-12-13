b349e00081d411d6a7eb3ddb4868b1d91520b184
datadavev <605409+datadavev@users.noreply.github.com>
2022-12-13 13:24:49 -0500

    refactoring for output

--- ezidapp/management/commands/diag-reserved.py
+++ ezidapp/management/commands/diag-reserved.py
@@ -9,9 +9,11 @@ at any time, including a running production instance.
 
 import argparse
 import csv
+import datetime
 import json
 import logging
-import datetime
+import time
+import urllib.parse
 import zlib
 
 import django.apps
@@ -21,20 +23,77 @@ import django.core.serializers
 import django.db.models
 import django.forms
 import django.forms.models
+import requests
+import requests.auth
 
 import ezidapp.models.datacenter
 import ezidapp.models.group
 import ezidapp.models.identifier
+import ezidapp.models.shoulder
 import ezidapp.models.user
-import impl.noid_egg
 
 log = logging.getLogger(__name__)
+
+DATACITE_REST_API = "https://api.datacite.org/dois/"
 
 class SplitArgs(argparse.Action):
     # From: https://stackoverflow.com/questions/52132076/argparse-action-or-type-for-comma-separated-list
     def __call__(self, parser, namespace, values, option_string=None):
         # Be sure to strip, maybe they have spaces where they don't belong and wrapped the arg value in quotes
         setattr(namespace, self.dest, [value.strip() for value in values.split(",")])
+
+class DataCiteREST:
+
+    def __init__(self, service_url:str=DATACITE_REST_API):
+        self._service_url = service_url.rstrip("/") + "/"
+        self._session = None
+        self._allocators = {
+            a: getattr(django.conf.settings, f"ALLOCATOR_{a}_PASSWORD")
+            for a in django.conf.settings.DATACITE_ALLOCATORS.split(",")
+        }
+
+    def _auth(self, datacenter:str)->requests.auth.HTTPBasicAuth:
+        allocator, center = datacenter.split(".", 1)
+        return requests.auth.HTTPBasicAuth(datacenter, self._allocators.get(allocator))
+
+    def getRecord(self, doi:str, datacenter:str)->dict:
+        if self._session is None:
+            self._session = requests.Session()
+        if doi.startswith("doi:"):
+            doi = doi[4:]
+        headers = {
+            "Accept":"application/vnd.api+json"
+        }
+        params = {}
+        t0 = time.time()
+        url = urllib.parse.urljoin(self._service_url, urllib.parse.quote(doi))
+        response = self._session.get(url, headers=headers, params=params, auth=self._auth(datacenter))
+        dt = time.time() - t0
+        log.debug("%s  %s",dt, url)
+        res = {
+            "status": response.status_code,
+            "meta":{'isActive':None, 'state': None, 'url': None}
+        }
+        if response.status_code == 200:
+            m = response.json()
+            a = m.get('data',{}).get('attributes', {})
+            res["meta"] = {
+                'isActive':a.get('isActive',False),
+                'state': a.get('state'),
+                'url':a.get('url'),
+            }
+        return res
+
+    def set_isActive(self, identifier:str, datacenter:str, active:bool=True):
+        data = {
+            "data": {
+                "type": "dois",
+                "attributes": {
+                    "isActive": active
+                }
+            }
+        }
+        pass
 
 class Command(django.core.management.BaseCommand):
     help = __doc__
@@ -74,124 +133,31 @@ class Command(django.core.management.BaseCommand):
             action='store_true',
             help='Show Identifier instead of SearchIdentifier table entry',
         )
-
-
-    def handle_show(self, *args, **opts):
-        def jsonable_instance(o):
-            if o is None:
-                return o
-            res = json.loads(
-                django.core.serializers.serialize(
-                    'json',
-                    [
-                        o,
-                    ],
-                )
-            )[0]
-            return res
-
-        def tstamp_to_text(t):
-            return datetime.datetime.fromtimestamp(t, tz=datetime.timezone.utc).isoformat()
-
-        expand_fields = ['datacenter', 'owner', 'ownergroup', 'profile']
-        identifier_class = ezidapp.models.identifier.SearchIdentifier
-        if opts["identifier"]:
-            identifier_class = ezidapp.models.identifier.Identifier
-        identifiers = identifier_class.objects.filter(identifier__in=opts["identifiers"])
-        if opts['expanded']:
-            identifiers = identifiers.select_related(*expand_fields)
-        entries = []
-        for identifier in identifiers:
-            # Note, it is far more efficient to just call serialize('json', identifiers, indent=2)
-            # but we want to futz around with the cm section and other fields for each instance.
-            entry = jsonable_instance(identifier)
-            entry["isAgentPid"] = identifier.isAgentPid
-            if opts["legacy"]:
-                # Get the "legacy" format, which is used for sending to N2T binder
-                entry["legacy"] = identifier.toLegacy()
-            if opts["expanded"]:
-                for field_name in expand_fields:
-                    entry["fields"][field_name] = jsonable_instance(getattr(identifier, field_name))
-            if opts["times"]:
-                entry["fields"]["createTime"] = tstamp_to_text(entry["fields"]["createTime"])
-                entry["fields"]["updateTime"] = tstamp_to_text(entry["fields"]["updateTime"])
-            if opts["cm"]:
-                try:
-                    _cm = json.loads(zlib.decompress(identifier.cm))
-                    entry['fields']['cm'] = _cm
-                    # Simple test to verify the decode cm section matches the metadata section
-                    _mequal = len(_cm.keys()) == len(entry['fields']['metadata'].keys())
-                    for k, v in _cm.items():
-                        if entry['fields']['metadata'][k] != _cm[k]:
-                            _mequal = False
-                            break
-                    entry["cm_eq_metadata"] = _mequal
-                except zlib.error:
-                    log.info("No cm section in %s", identifier.identifier)
-            n2t_meta = None
-            if opts["N2T"]:
-                # Retrieve entry from N2T
-                n2t_meta = impl.noid_egg.getElements(identifier.identifier)
-                entry["n2t"] = n2t_meta
-            if opts["sync"]:
-                _legacy = identifier.toLegacy()
-                # See proc_binder.update
-                # Retrieve the existing metadata from N2T
-                m = n2t_meta
-                if m is None:
-                    m = impl.noid_egg.getElements(identifier.identifier)
-                if m is None:
-                    m = {}
-                # First, update m with provided metadata
-                for k, v in list(_legacy.items()):
-                    # If the provided metadata matches existing, then ignore
-                    if m.get(k) == v:
-                        del m[k]
-                    # Otherwise add property to list for sending back to N2T
-                    else:
-                        m[k] = v
-                # If properties retrieved from N2T are not present in the supplied
-                # update metadata, then set the value of the field to an empty string.
-                # An empty value results in an "rm" (remove) operation for that field
-                # being sent to N2T.
-                for k in list(m.keys()):
-                    if k not in _legacy:
-                        m[k] = ""
-                if len(m) > 0:
-                    log.warning("Updating N2T metadata for %s", identifier.identifier)
-                    log.info("Pending updates for %s:\n%s", identifier.identifier, m)
-                    self.stdout.write(f"About to update {identifier.identifier} !")
-                    response = input("Enter Y to continue, anything else aborts: ")
-                    if response.strip() == 'Y':
-                        impl.noid_egg.setElements(identifier.identifier, m)
-                        ##
-                        # Retrieve the updated metadata and add to the entry
-                        entry["n2t_updated"] = impl.noid_egg.getElements(identifier.identifier)
-                    else:
-                        self.stdout.write("Aborted.")
-                else:
-                    log.info("No pending updates for %s", identifier.identifier)
-
-            entries.append(entry)
-        self.stdout.write(json.dumps(entries, indent=2, sort_keys=True))
+        _list.add_argument(
+            '-p',
+            '--prefix',
+            default=None,
+            help='Show identifiers starting with prefix (e.g. "doi:")'
+        )
+        _list.add_argument(
+            '-S',
+            '--status',
+            default='R',
+            help='Status code for identifier (R, P, U)'
+        )
+        _list.add_argument(
+            '-M',
+            '--metadata',
+            action='store_true',
+            help="Get properties from DataCite, CrossRef, or N2T"
+        )
 
     def handle_list(self, *args, **opts):
-        def jsonable_instance(o):
-            if o is None:
-                return o
-            res = json.loads(
-                django.core.serializers.serialize(
-                    'json',
-                    [
-                        o,
-                    ],
-                )
-            )[0]
-            return res
 
         def tstamp_to_text(t):
             return datetime.datetime.fromtimestamp(t, tz=datetime.timezone.utc).isoformat()
 
+        DC = DataCiteREST()
         t_start = int(datetime.datetime.fromisoformat(opts['start_time']).timestamp())
         t_end = int(datetime.datetime.fromisoformat(opts['end_time']).timestamp())
         expand_fields = ['datacenter', 'owner', 'ownergroup']
@@ -199,11 +165,17 @@ class Command(django.core.management.BaseCommand):
         identifier_class = ezidapp.models.identifier.SearchIdentifier
         if opts["identifier"]:
             identifier_class = ezidapp.models.identifier.Identifier
-        identifiers = identifier_class.objects.filter(status='R', updateTime__gte=t_start, updateTime__lt=t_end)
+        identifiers = identifier_class.objects.filter(status=opts.get('status','R'), updateTime__gte=t_start, updateTime__lt=t_end)
+        _prefix = opts.get("prefix", None)
+        if _prefix is not None:
+            identifiers = identifiers.filter(identifier__startswith=_prefix)
         identifiers = identifiers.select_related(*expand_fields)
 
         writer = csv.writer(self.stdout, dialect='excel')
-        writer.writerow(['PID','type','created','updated','owner','ownergroup','target'])
+        csv_header = ['PID','type','status','exported','profile','owner','ownergroup','datacenter','created','updated','target','resolvertarget']
+        if opts.get("metadata", False):
+            csv_header += ["dcrest","dcstate","dcactive","dcurl"]
+        writer.writerow(csv_header)
         for identifier in identifiers:
             itype = 'ark'
             if identifier.isDoi:
@@ -211,29 +183,35 @@ class Command(django.core.management.BaseCommand):
                     itype = 'crossref'
                 else:
                     itype = 'datacite'
+            datacenter = identifier.datacenter
+            if datacenter is None:
+                if identifier.isDatacite:
+                    _shoulder = ezidapp.models.shoulder.getLongestShoulderMatch(identifier.identifier)
+                    datacenter = _shoulder.datacenter.symbol
+            else:
+                datacenter = datacenter.symbol
+            m = {}
+            if opts.get("metadata", False):
+                if itype == "datacite":
+                    m = DC.getRecord(identifier.identifier, datacenter)
             row = [
                 identifier.identifier,
                 itype,
-                tstamp_to_text(identifier.createTime),
-                tstamp_to_text(identifier.updateTime),
+                identifier.status,
+                identifier.exported,
+                identifier.profile,
                 identifier.owner.username,
                 identifier.ownergroup.groupname,
+                datacenter,
+                tstamp_to_text(identifier.createTime),
+                tstamp_to_text(identifier.updateTime),
+                identifier.target,
                 identifier.resolverTarget,
             ]
+            if opts.get("metadata", False):
+                _m = m.get('meta', {})
+                row += [m.get('status',404),_m.get('state',None),_m.get('isActive',None), _m.get('url',None)]
             writer.writerow(row)
-
-        '''
-        dfields = _fields
-        if opts.get("compare", False):
-            dfields.append('n2t')
-        writer = csv.DictWriter(self.stdout, dfields, dialect='excel')
-        writer.writeheader()
-        for identifier in identifiers:
-            row = django.forms.models.model_to_dict(identifier, fields=_fields)
-            if opts.get('compare', False):
-                row['n2t'] = self.diff_n2t(identifier)
-            writer.writerow(row)
-        '''
 
     def handle(self, *args, **opts):
         operation = opts['operation']
