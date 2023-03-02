da0c1af4993c87de8611cb3f9f1b1ac28de3ec81
datadavev <605409+datadavev@users.noreply.github.com>
2023-03-02 15:21:40 -0500

    Code formatting

--- tests/resolve/test_resolver.py
+++ tests/resolve/test_resolver.py
@@ -10,73 +10,156 @@ _L = logging.getLogger("test_resolver")
     "val,expected",
     [
         (
-                "ark:/99999/fk412345/foo",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345/foo", scheme="ark", prefix="99999",
-                                                suffix="fk412345/foo")),
+            "ark:/99999/fk412345/foo",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345/foo",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345/foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345/foo%efg%1abar",
-                (
-                        impl.resolver.IdentifierStruct(
-                            original="ark:/99999/fk412345/foo%efg%1abar",scheme="ark", prefix="99999", suffix="fk412345/foo%EFg%1Abar"
-                        )
-                ),
+            "ark:/99999/fk412345/foo%efg%1abar",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345/foo%efg%1abar",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345/foo%EFg%1Abar",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk-4-1-2-3-4-5/f-o-o",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk-4-1-2-3-4-5/f-o-o",scheme="ark", prefix="99999", suffix="fk412345/foo")),
+            "ark:/99999/fk-4-1-2-3-4-5/f-o-o",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk-4-1-2-3-4-5/f-o-o",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345/foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345//foo",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345//foo",scheme="ark", prefix="99999", suffix="fk412345/foo")),
+            "ark:/99999/fk412345//foo",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345//foo",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345/foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345///foo",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345///foo",scheme="ark", prefix="99999", suffix="fk412345/foo")),
+            "ark:/99999/fk412345///foo",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345///foo",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345/foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345..foo",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345..foo",scheme="ark", prefix="99999", suffix="fk412345.foo")),
+            "ark:/99999/fk412345..foo",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345..foo",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345.foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345.//foo",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345.//foo",scheme="ark", prefix="99999", suffix="fk412345.foo")),
+            "ark:/99999/fk412345.//foo",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345.//foo",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345.foo",
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345.//foo?",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345.//foo?",scheme="ark", prefix="99999", suffix="fk412345.foo", inflection=True)),
+            "ark:/99999/fk412345.//foo?",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345.//foo?",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345.foo",
+                    inflection=True,
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345.//foo??",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345.//foo??",scheme="ark", prefix="99999", suffix="fk412345.foo", inflection=True)),
+            "ark:/99999/fk412345.//foo??",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345.//foo??",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345.foo",
+                    inflection=True,
+                )
+            ),
         ),
         (
-                "ark:/99999/fk412345.//foo?info",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/fk412345.//foo?info",scheme="ark", prefix="99999", suffix="fk412345.foo", inflection=True)),
+            "ark:/99999/fk412345.//foo?info",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/fk412345.//foo?info",
+                    scheme="ark",
+                    prefix="99999",
+                    suffix="fk412345.foo",
+                    inflection=True,
+                )
+            ),
         ),
         (
-                "ark:/99999/",
-                (impl.resolver.IdentifierStruct(original="ark:/99999/",scheme="ark", prefix="99999", suffix=None)),
+            "ark:/99999/",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999/", scheme="ark", prefix="99999", suffix=None
+                )
+            ),
         ),
         (
-                "ark:/99999",
-                (impl.resolver.IdentifierStruct(original="ark:/99999",scheme="ark", prefix="99999", suffix=None)),
+            "ark:/99999",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="ark:/99999", scheme="ark", prefix="99999", suffix=None
+                )
+            ),
         ),
         (
-                "99999/fk412345/foo",
-                (ValueError("99999/fk412345/foo has no scheme.")),
+            "99999/fk412345/foo",
+            (ValueError("99999/fk412345/foo has no scheme.")),
         ),
         (
-                "ark:",
-                (impl.resolver.IdentifierStruct(original="ark:",scheme="ark", prefix="")),
+            "ark:",
+            (impl.resolver.IdentifierStruct(original="ark:", scheme="ark", prefix="")),
         ),
         (
-                "ark:/",
-                (impl.resolver.IdentifierStruct(original="ark:/",scheme="ark", prefix="")),
+            "ark:/",
+            (impl.resolver.IdentifierStruct(original="ark:/", scheme="ark", prefix="")),
         ),
         (
-                "doi:10.5065/D6HM56KS",
-                (impl.resolver.IdentifierStruct(original="doi:10.5065/D6HM56KS",scheme="doi", prefix="10.5065", suffix="D6HM56KS")),
+            "doi:10.5065/D6HM56KS",
+            (
+                impl.resolver.IdentifierStruct(
+                    original="doi:10.5065/D6HM56KS",
+                    scheme="doi",
+                    prefix="10.5065",
+                    suffix="D6HM56KS",
+                )
+            ),
         ),
     ],
 )
@@ -97,10 +180,18 @@ def test_parse(val, expected):
 @pytest.mark.parametrize(
     "val,expected",
     [
-        ("ark:/88122/zqfw0190", ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190")),
-        ("ark:/88122/zqfw0190_extra", ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190")),
         (
-        "ark:88122/zq-fw-01-90_extra", ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190")),
+            "ark:/88122/zqfw0190",
+            ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190"),
+        ),
+        (
+            "ark:/88122/zqfw0190_extra",
+            ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190"),
+        ),
+        (
+            "ark:88122/zq-fw-01-90_extra",
+            ("ark:/88122/zqfw0190", "https://www.industrydocuments.ucsf.edu/docs/zqfw0190"),
+        ),
         ("ark:/88122/", (ezidapp.models.identifier.Identifier.DoesNotExist())),
     ],
 )
