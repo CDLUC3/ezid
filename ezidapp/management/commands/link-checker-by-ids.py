42ae3ec04d46422535cfde6760e148b3eb60da14
jsjiang <jing.jiang@ucop.edu>
2023-04-20 16:24:17 -0700

    create the http_client module

--- ezidapp/management/commands/link-checker-by-ids.py
+++ ezidapp/management/commands/link-checker-by-ids.py
@@ -17,7 +17,6 @@ import re
 import urllib.error
 import urllib.parse
 import urllib.request
-import requests
 import datetime
 import csv
 from typing import List, IO, Dict, Tuple, Union
@@ -29,6 +28,7 @@ import django.core.management
 import impl
 import impl.nog.util
 import impl.util
+import impl.http_client
 
 log = logging.getLogger(__name__)
 
@@ -107,7 +107,7 @@ class Command(django.core.management.BaseCommand):
                     output_dict['URL updated'] = "Yes"
                 if si_url:
                     output_dict['SI URL'] = si_url
-                    ret = self.check_url(si_url)
+                    ret = impl.http_client.check_url(si_url)
                     self.update_output_dict(output_dict, ret)
                 
                 csv_writer.writerow(output_dict)
@@ -125,7 +125,7 @@ class Command(django.core.management.BaseCommand):
         for url in url_list:
             output_dict = {}
             output_dict['URL'] = url
-            ret = self.check_url(url)
+            ret = impl.http_client.check_url(url)
             ret_0 = self.check_url_0(url)
             self.update_output_dict(output_dict, ret)
             self.update_output_dict_0(output_dict, ret_0)
@@ -208,90 +208,7 @@ class Command(django.core.management.BaseCommand):
   
         log.info("identifier file successfully loaded")
         return id_list, url_list
-
-
-    def check_url(self, url: str)->Dict[str, Union[str, int]]:
-        """Tests a target URL by performing a GET request on the URL.
-         
-            A timely non-excptional response equates to success.
-            Redirection is allowed by using the allow_redirect default setting.
-
-        Args:
-            url (str): target url to check
-
-        Returns:
-            Dict[str, Union[str, int]]: dict with folllowing keys to indicate for url checking status:
-                'returnCode',
-                'success',
-                'mimeType',
-                'content_size',
-                'error',
-        """
-        success = False
-        returnCode = -1
-        mimeType = "unknown"
-        content = b""
-        error = ""
-        content_size = 0
-        chunk_size = 1024*1024  # 1MB
-
-        try:
-            response = requests.get(
-                url=url,
-                headers={
-                    "User-Agent": django.conf.settings.LINKCHECKER_USER_AGENT,
-                    "Accept": "*/*",
-                },
-                timeout=django.conf.settings.LINKCHECKER_CHECK_TIMEOUT,
-                stream=True,
-            )
-            response.raise_for_status()
-            returnCode = response.status_code
-            mimeType = response.headers.get("Content-Type")
-            content_size = response.headers.get("Content-Length")
-
-            if content_size is None or content_size == 0:
-                for chunk in response.iter_content(chunk_size=chunk_size):
-                    if chunk:
-                        content += chunk
-                    if len(content) > django.conf.settings.LINKCHECKER_MAX_READ:
-                        log.info("Content size exceeded LINKCHECKER_MAX_READ")
-                        break
-            content_size = len(content)
-            success = True
-        except requests.exceptions.RequestException as e:
-            error = "RequestExceptio: " + str(e)[:200]
-            if hasattr(e, 'response') and e.response is not None:
-                returnCode = e.response.status_code
-                mimeType = e.response.headers.get("Content-Type")
-                content_size = e.response.headers.get("Content-Length")
-                if content_size is None:
-                    content_size = 0
-
-            # Note and code from ezid v.3.0
-            # Some servers deliver a complete HTML document, but,
-            # apparently expecting further requests from a web browser
-            # that never arrive, hold the connection open and ultimately
-            # deliver a read failure. We consider these cases successes.
-            if mimeType.startswith("text/html") and re.search(
-                "</\s*html\s*>\s*$", str(content, 'utf-8'), re.I
-            ):
-                success = True
-                log.info("Received complete HTML page when error occurred: " + error)
-            else:
-                log.exception(error)
-        except Exception as e:
-            error = "Exception: " + str(e)[:200]
-
-        ret_dict = {
-            'returnCode': returnCode,
-            'success': success,
-            'mimeType': mimeType,
-            'content_size': content_size,
-            'error': error,
-        }
-        return ret_dict
-
+    
     def check_url_0(self, target):
         o = urllib.request.build_opener(
             urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar()),
---
+++ impl/http_client.py
@@ -0,0 +1,90 @@
+import re
+import requests
+from typing import Dict, Union
+import logging
+
+import django.conf
+
+log = logging.getLogger(__name__)
+
+def check_url(url: str)->Dict[str, Union[str, int]]:
+    """Tests a target URL by performing a GET request on the URL.
+    
+        A timely non-excptional response equates to success.
+        Redirection is allowed by using the allow_redirect default setting.
+
+    Args:
+        url (str): target url to check
+
+    Returns:
+        Dict[str, Union[str, int]]: dict with folllowing keys to indicate for url checking status:
+            'returnCode',
+            'success',
+            'mimeType',
+            'content_size',
+            'error',
+    """
+    success = False
+    returnCode = -1
+    mimeType = "unknown"
+    content = b""
+    error = ""
+    content_size = 0
+    chunk_size = 1024*1024  # 1MB
+
+    try:
+        response = requests.get(
+            url=url,
+            headers={
+                "User-Agent": django.conf.settings.LINKCHECKER_USER_AGENT,
+                "Accept": "*/*",
+            },
+            timeout=django.conf.settings.LINKCHECKER_CHECK_TIMEOUT,
+            stream=True,
+        )
+        response.raise_for_status()
+        returnCode = response.status_code
+        mimeType = response.headers.get("Content-Type")
+        content_size = response.headers.get("Content-Length")
+
+        if content_size is None or content_size == 0:
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    content += chunk
+                if len(content) > django.conf.settings.LINKCHECKER_MAX_READ:
+                    log.info("Content size exceeded LINKCHECKER_MAX_READ")
+                    break
+        content_size = len(content)
+        success = True
+    except requests.exceptions.RequestException as e:
+        error = "RequestExceptio: " + str(e)[:200]
+        if hasattr(e, 'response') and e.response is not None:
+            returnCode = e.response.status_code
+            mimeType = e.response.headers.get("Content-Type")
+            content_size = e.response.headers.get("Content-Length")
+            if content_size is None:
+                content_size = 0
+
+        # Note and code from ezid v.3.0
+        # Some servers deliver a complete HTML document, but,
+        # apparently expecting further requests from a web browser
+        # that never arrive, hold the connection open and ultimately
+        # deliver a read failure. We consider these cases successes.
+        if mimeType.startswith("text/html") and re.search(
+            "</\s*html\s*>\s*$", str(content, 'utf-8'), re.I
+        ):
+            success = True
+            log.info("Received complete HTML page when error occurred: " + error)
+        else:
+            log.exception(error)
+    except Exception as e:
+        error = "Exception: " + str(e)[:200]
+
+    ret_dict = {
+        'returnCode': returnCode,
+        'success': success,
+        'mimeType': mimeType,
+        'content_size': content_size,
+        'error': error,
+    }
+    return ret_dict
