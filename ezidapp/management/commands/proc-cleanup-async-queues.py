28719a845a211a313775860ffc07557e0803536c
rushirajnenuji <rushirajnenuji@gmail.com>
2023-06-28 10:36:08 -0700

    Add deletion logic to clean up async queues
    Add deletion logic to clean up async queues

--- ezidapp/management/commands/proc-cleanup-async-queues.py
+++ ezidapp/management/commands/proc-cleanup-async-queues.py
@@ -3,21 +3,18 @@
 #  Copyright©2021, Regents of the University of California
 #  http://creativecommons.org/licenses/BSD
 
-"""Clean up entries that are successfully completed or are a 'no-op'
+"""
+
+Clean up entries that are successfully completed or are a 'no-op'
 
 Identifier operation entries are retrieved by querying the database;
 operations that successfully completed or are a no-op are deleted based on
 pre-set interval.
 
-Iden
 """
 
 import logging
 import time
-import urllib.error
-import urllib.parse
-import urllib.request
-import urllib.response
 
 import django.conf
 import django.conf
@@ -26,61 +23,121 @@ import django.db.transaction
 
 import ezidapp.management.commands.proc_base
 import ezidapp.models.identifier
-import ezidapp.models.identifier
-import ezidapp.models.news_feed
 import ezidapp.models.shoulder
 from django.db.models import Q
 
-import impl.enqueue
-import impl.ezid
-
 log = logging.getLogger(__name__)
-
 
 class Command(ezidapp.management.commands.proc_base.AsyncProcessingCommand):
     help = __doc__
     name = __name__
-    # TODO: set up flag in J2 template
+
     setting = 'DAEMONS_QUEUE_CLEANUP_ENABLED'
+
     queueType = {
         'binder': ezidapp.models.async_queue.BinderQueue,
         'crossref': ezidapp.models.async_queue.CrossrefQueue,
         'datacite': ezidapp.models.async_queue.DataciteQueue,
         'search': ezidapp.models.async_queue.SearchIndexerQueue
     }
+
     refIdentifier = ezidapp.models.identifier.RefIdentifier
 
     def __init__(self):
         super().__init__()
 
+
     def run(self):
+        """
+            Checks for the successfully processed identifier
+
+            Args:
+                None
+        """
+        # keep running until terminated
         keepRunning = True
         while keepRunning:
-            for key, value in self.queueType.items():
-                log.info("Running job for " + key)
-                queue = value
+            currentTime=int(time.time())
+            timeDelta=django.conf.settings.DAEMONS_CHECK_IDENTIFIER_ASYNC_STATUS_TIMESTAMP
 
-                qs = queue.objects.filter(
-                    Q(status=queue.SUCCESS)
-                    | Q(status=queue.IGNORED)
-                ).order_by("seq")[: django.conf.settings.DAEMONS_MAX_BATCH_SIZE]
+            # retrieve identifiers with update timestamp within a set range
+            refIdsQS = self.refIdentifier.objects.filter(
+                updateTime__lte=currentTime,
+                updateTime__gte=currentTime - timeDelta
+            ).order_by("-pk")[: django.conf.settings.DAEMONS_MAX_BATCH_SIZE]
 
-                if not qs:
-                    self.sleep(django.conf.settings.DAEMONS_IDLE_SLEEP)
-                    continue
+            log.info("Checking ref Ids in the range: " + str(currentTime) + " - " + str(currentTime - timeDelta))
 
-                for task_model in qs:
-                    log.info('-' * 100)
-                    log.info(f'Processing task: {str(task_model)}')
-                    self.deleteRecord(queue, task_model)
+            # iterate over query set to check each identifier status
+            for refId in refIdsQS:
+
+                # set status for each handle system
+                identifierStatus = {
+                    'binder' : False,
+                    'crossref' : False,
+                    'datacite' : False,
+                    'search' : False
+                }
+
+                # check if the identifier is processed for each background job 
+                for key, value in self.queueType.items():
+                    queue = value
+
+                    qs = queue.objects.filter(
+                        Q(refIdentifier_id=refId.pk)
+                    )
+
+                    # if the identifier does not exist in the table
+                    # mark as 'OK' to delete from the refIdentifier
+                    if not qs:
+                        identifierStatus[key] = True
+                        self.sleep(django.conf.settings.DAEMONS_IDLE_SLEEP)
+                        continue
+
+                    for task_model in qs:
+                        log.info('-' * 10)
+                        log.info("Running job for identifier: " + refId.identifier + " in " + key + " queue")
+
+                        # delete identifier if the status is successfully synced or
+                        # not applicable for this handle system
+                        if (task_model.status==queue.SUCCESS or task_model.status==queue.IGNORED):
+                            log.info(
+                                "Delete identifier: " + refId.identifier + " in " + key + " queue")
+                            identifierStatus[key] = True
+                            self.deleteRecord(queue, task_model.pk, record_type=key, identifier=refId.identifier)
+
+                # if the identifier is successfully processed for all the handle system
+                # delete it from the refIdentifier table
+                if all(i for i in identifierStatus.values()):
+                    log.info(
+                        "Delete identifier: " + refId.identifier + " from refIdentifier table.")
+                    self.deleteRecord(self.refIdentifier, refId.pk, record_type='refId', identifier=refId.identifier)
 
             keepRunning=False
 
-            # TODO: set up flag in J2 template
             self.sleep(django.conf.settings.DAEMONS_BATCH_SLEEP)
 
-    def deleteRecord(self, queue, task_model):
+    def deleteRecord(self, queue, primary_key, record_type=None, identifier=None):
+        """
+            Deletes the identifier record that has been successfully completed
+            based on the record's primary key provided
+
+        Args:
+            queue : async handle queue
+            primary_key (str): primary key of the record to be deleted.
+            record_type (str): . Defaults to None.
+            identifier (str): . Defaults to None.
+        """
         try:
-            queue.objects.filter(seq=task_model.pk).delete()
+            # check if the record to be deleted is a refIdentifier record
+            if (record_type is not None and record_type == 'refId'):
+                log.info(type(queue))
+                log.info("Delete refId: " + str(primary_key))
+                queue.objects.filter(id=primary_key).delete()
+            else:
+                log.info("Delete async entry: " + str(primary_key))
+                queue.objects.filter(seq=primary_key).delete()
         except Exception as e:
+            log.error("Exception occured while processing identifier '" + identifier + "' for '" +
+                        record_type + "' table")
             log.error(e)
