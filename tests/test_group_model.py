b3507adf9e9c0031c9d365eac7254dec01d26aca
jsjiang <jing.jiang@ucop.edu>
2023-10-16 15:03:25 -0700

    test create user and group with minted IDs

---
+++ tests/test_group_model.py
@@ -0,0 +1,52 @@
+#  Copyright©2021, Regents of the University of California
+#  http://creativecommons.org/licenses/BSD
+
+"""Test the minter data modle
+"""
+import pytest
+import logging
+from django.core.exceptions import ValidationError
+
+import django.conf
+import ezidapp.models.group
+
+log = logging.getLogger(__name__)
+
+class TestGropu:
+    def test_1_create_group(self, caplog):
+        """test - Create group with group ID minted on Agent shoulder"""
+        caplog.set_level(logging.INFO)
+
+        next_spin = '154dv8s'
+        expected_pid = f'{django.conf.settings.SHOULDERS_AGENT}{next_spin}'
+
+        assert not ezidapp.models.group.Group.objects.filter(
+            pid=expected_pid
+        ).exists()
+
+        group = ezidapp.models.group.Group(
+            groupname='testgroup',
+            organizationName = "Test Group",
+            realm_id = 1,
+            )
+        with pytest.raises(ValidationError) as exc_info:
+             group.full_clean()
+
+        group.save()
+
+        assert ezidapp.models.group.Group.objects.filter(
+            pid=expected_pid
+        ).exists()
+
+        group = ezidapp.models.group.Group.objects.get(pid=expected_pid)
+
+        assert group.pid == expected_pid
+        assert group.groupname == 'testgroup'
+        assert group.organizationName == 'Test Group'
+        assert group.realm_id == 1
+
+
+        
+       
+
+    
