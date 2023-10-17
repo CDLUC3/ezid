513265b699ea559b87e45a4b419564540a425cb1
jsjiang <jing.jiang@ucop.edu>
2023-10-17 10:54:17 -0700

    script for prepare a test-db

---
+++ prepare_test_db.sh
@@ -0,0 +1,13 @@
+mysql -h127.0.0.1 -uroot -p << EOF
+  drop database if exists ezid_test_db;
+  create database ezid_test_db;
+  use ezid_test_db;
+  drop user if exists 'ezid_test_user'@'%';
+  create user 'ezid_test_user'@'%' identified by 'ezid_test_pw';
+  grant all privileges on ezid_test_db.* to 'ezid_test_user'@'%';
+EOF
+
+
+python ./manage.py migrate
+
+python ./manage.py loaddata db
