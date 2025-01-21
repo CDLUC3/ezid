f878be3279d618f1264717e4d999923a59abe43e
sfisher <scott.fisher@ucop.edu>
2025-01-21 14:02:15 -0800

    I'm not sure why this shows a delete and then adding again, but it seems to now work.

--- dev/js/main2.js
+++ dev/js/main2.js
@@ -1,7 +1,7 @@
 // ##### Main JavaScript ##### //
 
-$(document).ready(function(){
-  
+$(document).ready(function () {
+
   // ***** Show/hide mobile menu via mobile menu icon ***** //
 
   // Before toggling menu, change default header menu class to non-selected state and change default aria attribute:
@@ -12,11 +12,11 @@ $(document).ready(function(){
   $('#js-header__nav').attr('aria-expanded', 'false');
 
   // Toggle classes and attributes:
-  $('#js-header__nav-button').click(function(){
-    
+  $('#js-header__nav-button').click(function () {
+
     $('#js-header__nav').toggleClass('header__nav header__nav--selected', 300, 'easeInOutCubic');
 
-    if($('#js-header__nav').attr('aria-expanded') == 'false') {
+    if ($('#js-header__nav').attr('aria-expanded') == 'false') {
       $('#js-header__nav').attr('aria-expanded', 'true');
     } else {
       $('#js-header__nav').attr('aria-expanded', 'false');
@@ -28,11 +28,11 @@ $(document).ready(function(){
 
   // Pre-open an accordion section by retrieving the referring URL hash (up to 2 digits), removing the hash tag, and adding the hash value to the jQuery selector:
 
-  var urlhash = window.location.hash.substr(1,3);
-  
-  $('#accordion__section-'+urlhash).attr('open', '');
+  var urlhash = window.location.hash.substr(1, 3);
 
-  $('.accordion__title').click(function(){
+  $('#accordion__section-' + urlhash).attr('open', '');
+
+  $('.accordion__title').click(function () {
 
     // If an accordion title is clicked, close all the other sections if they are open and set their aria-expanded attributes to false:
     if ($(this).parent().siblings().attr('open', '')) {
@@ -51,11 +51,11 @@ $(document).ready(function(){
 
   // If 'required' attribute exists on a text input, add 'required' class to its label:
 
-  $('.fcontrol__text-field-stacked[required]').map(function() {
+  $('.fcontrol__text-field-stacked[required]').map(function () {
     $(this).siblings('.fcontrol__text-label-stacked').addClass('fcontrol__label-required');
   });
 
-  $('.fcontrol__text-field-inline[required]').map(function() {
+  $('.fcontrol__text-field-inline[required]').map(function () {
     $(this).siblings('.fcontrol__text-label-inline').addClass('fcontrol__label-required');
   });
 
@@ -63,10 +63,10 @@ $(document).ready(function(){
 
   // Toggle open and closed from login button
 
-  $('#js-header__loginout-button').click(function(){
+  $('#js-header__loginout-button').click(function () {
     if ($('#js-login-modal').attr('aria-hidden') == 'true') {
       $('#js-login-modal').attr('aria-hidden', 'false');
-    }else {
+    } else {
       $('#js-login-modal').attr('aria-hidden', 'true');
     }
     $('#js-login-modal').fadeToggle(200);
@@ -74,16 +74,69 @@ $(document).ready(function(){
 
   // Close when close icon is clicked
 
-  $('#js-login-modal__close').click(function(){
+  $('#js-login-modal__close').click(function () {
     $('#js-login-modal').attr('aria-hidden', 'true');
     $('#js-login-modal').fadeToggle(200);
   });
 
   // Close when form is submitted
 
-  $('#js-login-modal__form').submit(function(){
+  $('#js-login-modal__form').submit(function () {
     $('#js-login-modal').attr('aria-hidden', 'true');
     $('#js-login-modal').fadeToggle(200);
   });
 
 }); // Close $(document).ready(function()
+
+// ***** The "Learn" menu needs more flexibility for keyboard navigation ***** //
+
+document.addEventListener('DOMContentLoaded', () => {
+  const detailsElement = document.getElementById('header__nav-details-learn');
+  const summaryElement = detailsElement.querySelector('summary');
+
+  // Close menu when clicking outside
+  document.addEventListener('click', (event) => {
+    if (!detailsElement.contains(event.target)) {
+      detailsElement.removeAttribute('open');
+      detailsElement.setAttribute('aria-expanded', 'false');
+    }
+  });
+
+  // Toggle aria-expanded when menu is toggled
+  summaryElement.addEventListener('click', () => {
+    const isOpen = detailsElement.hasAttribute('open');
+    detailsElement.setAttribute('aria-expanded', String(!isOpen));
+  });
+
+  // Keyboard navigation
+  detailsElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      detailsElement.removeAttribute('open');
+      detailsElement.setAttribute('aria-expanded', 'false');
+      summaryElement.focus();
+    }
+  });
+
+  // Manage focus within menu
+  const focusableElements = detailsElement.querySelectorAll('a');
+  detailsElement.addEventListener('keydown', (event) => {
+    const focusArray = Array.from(focusableElements);
+    const currentIndex = focusArray.indexOf(document.activeElement);
+
+    if (event.key === 'Tab') {
+      if (event.shiftKey) {
+        // Tab backwards
+        if (currentIndex === 0 || currentIndex === -1) {
+          event.preventDefault();
+          focusArray[focusArray.length - 1].focus();
+        }
+      } else {
+        // Tab forwards
+        if (currentIndex === focusArray.length - 1) {
+          event.preventDefault();
+          focusArray[0].focus();
+        }
+      }
+    }
+  });
+});
